a61df5e8e76ad1a3b0e5a00c058b876c7414c5f7
KANISHKA R <kanishka.r221@gmail.com>
2025-11-09 19:33:19 +0530

    Update 01. Queue_Unbounded.sv

--- DAY_2/QUEUE/01. Queue_Unbounded.sv
+++ DAY_2/QUEUE/01. Queue_Unbounded.sv
@@ -1,1 +1,27 @@
-
+module queue;
+  int j;
+  int q[$] = '{0,2,5};
+  initial begin
+    j=1;
+    q.insert(1,j);
+    $display(q);
+    q.delete(1);
+    $display(q);
+    q.push_front(6);
+    $display(q);
+    q.push_back(7);
+    $display(q);
+	j=q.pop_front( );
+    $display(j,",",q);
+    j=q.pop_back( );
+    $display(j,",",q);
+    q.reverse( );
+    $display(q);
+    q.sort( );
+    $display(q);
+    q.rsort();
+    $display(q);
+    q.shuffle( );
+    $display(q);
+  end
+endmodule
