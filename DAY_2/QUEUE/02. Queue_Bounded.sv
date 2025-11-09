0dac97ecd491f06979e5dba82fe669af3117f639
KANISHKA R <kanishka.r221@gmail.com>
2025-11-09 19:39:47 +0530

    Update 02. Queue_Bounded.sv

--- DAY_2/QUEUE/02. Queue_Bounded.sv
+++ DAY_2/QUEUE/02. Queue_Bounded.sv
@@ -1,1 +1,27 @@
-
+module queue;
+  int j;
+  int q[$:2] = '{0,2,5};
+  initial begin
+    j=1;
+    q.insert(1,j);
+    $display(q);
+    q.delete(1);
+    $display(q);
+    q.push_front(6);
+    $display(q);
+    q.push_back(7);
+    $display(q);
+    q.pop_front( );
+    $display(q);
+    q.pop_back( );
+    $display(q);
+	q.reverse( );
+    $display(q);
+    q.sort( );
+    $display(q);
+    q.rsort();
+    $display(q);
+    q.shuffle( );
+    $display(q);
+  end
+endmodule
