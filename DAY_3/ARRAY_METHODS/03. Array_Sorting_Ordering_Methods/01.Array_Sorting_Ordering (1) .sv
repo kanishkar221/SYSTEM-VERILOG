69dda1d20a9ee2a6a52f720d8416e68cde8a9775
KANISHKA R <kanishka.r221@gmail.com>
2025-11-10 21:21:18 +0530

    Update 01.Array_Sorting_Ordering (1) .sv

--- DAY_3/ARRAY_METHODS/03. Array_Sorting_Ordering_Methods/01.Array_Sorting_Ordering (1) .sv
+++ DAY_3/ARRAY_METHODS/03. Array_Sorting_Ordering_Methods/01.Array_Sorting_Ordering (1) .sv
@@ -1,1 +1,14 @@
-
+module array_methods;
+  int array[7] = '{2,7,5,8,1,9,4};
+  initial begin
+    array.reverse();
+    $display("reverse:%p",array);
+    array.sort();
+    $display("sort:%p",array);
+    array.rsort();
+    $display("rsort:%p",array);
+    array.shuffle();
+    $display("shuffle: %p",array);
+  end
+endmodule
+  
