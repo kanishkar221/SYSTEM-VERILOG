67ea7b3b4502527a08464bfcf6c36ee370826a36
KANISHKA R <kanishka.r221@gmail.com>
2025-11-10 21:13:15 +0530

    Update 02. Array_Reduction (2) .sv

--- DAY_3/ARRAY_METHODS/01.Array_Reduction_Methods/02. Array_Reduction (2) .sv
+++ DAY_3/ARRAY_METHODS/01.Array_Reduction_Methods/02. Array_Reduction (2) .sv
@@ -1,1 +1,15 @@
-
+module array_reduction;
+  int array[4] = '{1,2,3,4};
+  int sum;
+  initial begin
+    foreach (array[i])
+      array[i] = i;
+    $display("sum",array.sum);
+    $display("product",array.product);
+    $display("or",array.or);
+    $display("and",array.and);
+    $display("xor",array.xor);
+    sum=array.sum;
+    $display("sum",sum);
+  end
+endmodule
