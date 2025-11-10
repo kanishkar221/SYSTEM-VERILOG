2cdf9b470030f6f0352fb58e456d5beebb5ec01f
KANISHKA R <kanishka.r221@gmail.com>
2025-11-10 17:56:58 +0530

    Update 02. Dynamic Array Ordering .sv

--- DAY_2/DYNAMIC ARRAY/02. Dynamic Array Ordering .sv
+++ DAY_2/DYNAMIC ARRAY/02. Dynamic Array Ordering .sv
@@ -1,1 +1,17 @@
-
+module array;
+  int array[] = '{9,5,7,3,8,2,1};
+  initial begin
+    $display("size ",array.size());
+    array.reverse();
+    $display(array);
+    array.sort();
+    $display(array);
+    array.rsort();
+    $display(array);
+    array.shuffle();
+    $display(array);
+    array.delete();
+    $display(array);
+    $display("size ",array.size());
+  end
+endmodule
