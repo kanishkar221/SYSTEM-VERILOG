0e89ffcceb32bbda8e89efaa77181fdb8a0bc2f3
KANISHKA R <kanishka.r221@gmail.com>
2025-11-11 22:22:29 +0530

    Update 04. Queue Array .sv

--- ASSIGNMENTS/Assignment_1/04. Queue Array .sv
+++ ASSIGNMENTS/Assignment_1/04. Queue Array .sv
@@ -1,1 +1,16 @@
-
+module queue;
+  int q[$] = '{2,3,4,5};
+  initial begin
+    $display("Size of the queue", q.size());
+    q.insert(0,1);
+    $display("After insert", q);
+    q.delete(2);
+    $display("After delete", q);
+    q.push_back(9);
+    $display("After push_back", q);
+    q.shuffle();
+    $display("After shuffle", q);
+    q.reverse();
+    $display("After reverse", q);
+  end
+endmodule
