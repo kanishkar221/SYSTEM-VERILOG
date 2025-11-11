d0ad6d4f2f5a3d658df263e0a3ba0194ecfeed34
KANISHKA R <kanishka.r221@gmail.com>
2025-11-11 22:20:56 +0530

    Update 03. Dynamic Array using foreach .sv

--- ASSIGNMENTS/Assignment_1/03. Dynamic Array using foreach .sv
+++ ASSIGNMENTS/Assignment_1/03. Dynamic Array using foreach .sv
@@ -1,1 +1,13 @@
+module dynamic_array;
+  int array[];
+  initial begin 
+    array = new[5];
+    array='{1,2,3,4,5};
+    foreach (array[i])
+      $display("array",i,array[i]);
+    $display("Size of array ", array.size());
+    array.shuffle();
+    $display("After shuffling = %p",array);
+  end
+endmodule
 
