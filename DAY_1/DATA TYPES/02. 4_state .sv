80f25ad49fd31dbcb2493ede064298e9dd957610
KANISHKA R <kanishka.r221@gmail.com>
2025-11-12 22:23:48 +0530

    Update 02. 4_state .sv

--- DAY_1/DATA TYPES/02. 4_state .sv
+++ DAY_1/DATA TYPES/02. 4_state .sv
@@ -1,1 +1,21 @@
+module datatype;
+  logic [3:0] a;
+  logic [7:0] b;
+  logic signed [7:0] c;
+  logic [7:0] d;
 
+  initial begin
+    a = 4'bxz11;         
+    b = 8'hFF;           
+    c = 8'd10;          
+    d = 8'bzzxx_1010;    
+  end
+
+  initial begin
+    $display("a = %b", a);
+    $display("b = %b", b);
+    $display("c = %0d", c);
+    $display("d = %b", d);
+    #10 $finish;
+  end
+endmodule
