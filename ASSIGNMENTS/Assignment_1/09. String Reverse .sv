bc29e38ab254bbbe7bed619487189bb2f2e83289
KANISHKA R <kanishka.r221@gmail.com>
2025-11-12 16:29:52 +0530

    Update 09. String Reverse .sv

--- ASSIGNMENTS/Assignment_1/09. String Reverse .sv
+++ ASSIGNMENTS/Assignment_1/09. String Reverse .sv
@@ -1,1 +1,11 @@
-
+module reverse_string;
+  string s;
+  string rev_s;
+  initial begin
+    s = "SystemVerilog";
+    $display("Before the reverse = %s", s);
+    for (int i = s.len() - 1; i >= 0; i--) 
+      rev_s = {rev_s, s[i]};
+    $display("After the reverse = %s", rev_s);
+  end
+endmodule
