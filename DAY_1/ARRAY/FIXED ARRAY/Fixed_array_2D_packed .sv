7c96cef064596501ee379524cb0125d9ec44e918
KANISHKA R <kanishka.r221@gmail.com>
2025-11-12 22:02:04 +0530

    Update Fixed_array_2D_packed .sv

--- DAY_1/ARRAY/FIXED ARRAY/Fixed_array_2D_packed .sv
+++ DAY_1/ARRAY/FIXED ARRAY/Fixed_array_2D_packed .sv
@@ -1,1 +1,10 @@
-
+module tb;
+  bit [3:0][7:0] m_data;
+  initial begin
+    m_data=32'h01020304;
+    $display("m_data=%0h",m_data);
+    for(int i=0;i<4;i++)begin
+      $display("data[%0d] = %0h",i,m_data[i]);
+      end
+  end
+endmodule
