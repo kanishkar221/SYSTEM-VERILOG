18059c41c268f6491bfd3d7a0735b6a1a82a7b8a
KANISHKA R <kanishka.r221@gmail.com>
2025-11-12 16:28:21 +0530

    Update 08. Numeric string to integer .sv

--- ASSIGNMENTS/Assignment_1/08. Numeric string to integer .sv
+++ ASSIGNMENTS/Assignment_1/08. Numeric string to integer .sv
@@ -1,1 +1,20 @@
+module string_int;
+  string s;
+  int i;
+  initial begin
+    s = "12345ff12";
+    i = s.atoi();
+    $display("String to integer" , i);
+    i = i+100;
+    $display("i+100",i);
+    i = i-50;
+    $display("i-50",i);
+    i = i*2;
+    $display("i*2",i);
+    i = i/2;
+    $display("i/2",i);
+    i = i%2;
+    $display("i%2",i);
+  end
+endmodule
 
