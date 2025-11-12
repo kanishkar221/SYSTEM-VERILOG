d91cd526ba67d95590640162dd7a711541920a43
KANISHKA R <kanishka.r221@gmail.com>
2025-11-12 22:35:49 +0530

    Update Mixed_aray .sv

--- DAY_1/ARRAY/MIXED ARRAY/Mixed_aray .sv
+++ DAY_1/ARRAY/MIXED ARRAY/Mixed_aray .sv
@@ -1,1 +1,8 @@
-
+module mixed_array;
+  bit [3:0] array[2:0][2:0];
+  initial begin
+    array = '{'{4'h1,4'h2,4'h3},'{4'h4,4'h5,4'h6},'{4'h7,4'h8,4'h9}};
+    foreach (array[i,j])
+      $display("array[%0d][%0d] = %0h",i,j,array[i][j]);
+  end
+endmodule
