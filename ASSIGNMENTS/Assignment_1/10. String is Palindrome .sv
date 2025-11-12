dab48f0d676246318e0e29ed7429a0dc240be395
KANISHKA R <kanishka.r221@gmail.com>
2025-11-12 16:31:21 +0530

    Update 10. String is Palindrome .sv

--- ASSIGNMENTS/Assignment_1/10. String is Palindrome .sv
+++ ASSIGNMENTS/Assignment_1/10. String is Palindrome .sv
@@ -1,1 +1,15 @@
-
+module palindrome_check;
+  string s;
+  string rev_s;
+  initial begin
+    s = "racecar";
+    $display("Before the reverse = %s", s);
+    for (int i = s.len() - 1; i >= 0; i--)
+      rev_s = {rev_s, s[i]};
+    $display("After the reverse = %s", rev_s);
+    if (s == rev_s)
+      $display("String is a palindrome = %s", s);
+    else
+      $display("String is not a palindrome = %s", s);
+  end
+endmodule
