474e9994b2261b00d9221668fd2f1843aaea7299
KANISHKA R <kanishka.r221@gmail.com>
2025-11-13 16:10:20 +0530

    Add unpacked structure module with display logic
    Define an unpacked structure with fields and display values.

--- DAY_4/STRUCTURE/02. Structure_unpacked .sv
+++ DAY_4/STRUCTURE/02. Structure_unpacked .sv
@@ -1,1 +1,15 @@
-
+module unpacked_structure;
+  struct {              //unpacked structure
+    bit [7:0] intr;
+    logic [23:0] addr;
+  } SUR;
+  
+  initial begin
+    SUR.intr = 'hFF;             //single field
+    $display("SUR = %p",SUR);
+    $display("SUR intr = %h",SUR.intr);
+    
+    SUR = '{'h00,'hFF};          //all fields
+    $display("SUR = %p",SUR);
+  end
+endmodule
