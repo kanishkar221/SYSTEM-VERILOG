1785debc846d7a3c47ad2b946c2349314eb2e269
KANISHKA R <kanishka.r221@gmail.com>
2025-11-13 16:04:41 +0530

    Add packed structure module with initial values

--- DAY_4/STRUCTURE/01. Structure_packed .sv
+++ DAY_4/STRUCTURE/01. Structure_packed .sv
@@ -1,1 +1,20 @@
-
+module packed_structure;
+  struct packed {                    //packed structure
+    bit [7:0] intr;
+    logic [23:0] addr;
+  } SUR;
+  
+  initial begin
+    SUR.intr = 'hFX;
+    $display("SUR = %h",SUR);
+    $display("SUR intr = %h",SUR.intr);
+    
+    SUR = '{'h00,'hFF_FF_FF};
+    $display("SUR = %h",SUR);
+    
+    SUR = 0;
+    SUR = SUR + 'h15;
+    $display("SUR = %h",SUR);
+  end
+endmodule          
+             
