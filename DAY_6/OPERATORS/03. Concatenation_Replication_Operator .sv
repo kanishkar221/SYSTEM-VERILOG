b209e1c059c8bd11f2c5ac5b7195d770065e54f6
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:21:41 +0530

    Implement concatenation and replication operators in Verilog

--- DAY_6/OPERATORS/03. Concatenation_Replication_Operator .sv
+++ DAY_6/OPERATORS/03. Concatenation_Replication_Operator .sv
@@ -1,1 +1,16 @@
-
+module operators;
+  logic [0:3] a,b,c;
+  int x;
+  initial begin
+    a = 4'b1001;   
+    b = 4'b0110; 
+    c = 4'b1111;
+    $display("Value of a is %0b",a);
+    $display("Value of b is %0b",b);
+    $display("Value of c is %0b",c);
+    x = {a,b,c};
+    $display("Output of Concatenation operators %0b",x);
+    x = {a,{2{b}},c};
+    $display("Output of Replication operators %0b",x);
+  end
+endmodule
