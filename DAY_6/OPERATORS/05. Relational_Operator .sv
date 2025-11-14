95e8fb3bb5f050e93214c41e2759460dcc6e3e44
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:20:10 +0530

    Add relational operator module with comparisons

--- DAY_6/OPERATORS/05. Relational_Operator .sv
+++ DAY_6/OPERATORS/05. Relational_Operator .sv
@@ -1,1 +1,10 @@
-
+module operator;
+  int a = 10;
+  int b = 20;
+  initial begin
+    $display("a<b = %0d", a<b);
+    $display("a>b = %0d", a>b);
+    $display("a<=b = %0d", a<=b);
+    $display("a>=b = %0d", a>=b);
+  end
+endmodule
