f739ff122c29b586918925acdbb5afd88b3d3570
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:18:34 +0530

    Update 07. Equality_Operator .sv

--- DAY_6/OPERATORS/07. Equality_Operator .sv
+++ DAY_6/OPERATORS/07. Equality_Operator .sv
@@ -1,1 +1,24 @@
+module operators;
+  logic [3:0] a, b, c, y;
+  initial begin 
+    a = 4'b1101;
+    b = 4'b1001;
+    c = 4'b1101;
 
+    $display("Value of the a is %0b", a);
+    $display("Value of the b is %0b", b);
+    $display("Value of the c is %0b", c);
+
+    y = (a == b);
+    $display("Output of logical equality operator of a==b is %0b", y);
+   
+    y = (a != b);
+    $display("Output of logical inequality operator of a!=b is %0b", y);
+
+    y = (a === c);
+    $display("Output of case equality operator of a===c is %0b", y);
+
+    y = (a !== c);
+    $display("Output of case inequality operator of a!==c is %0b", y);
+  end
+endmodule
