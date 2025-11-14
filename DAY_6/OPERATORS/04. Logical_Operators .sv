723c107ce8b08173f9b977e981a1835d2ebdd496
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:21:05 +0530

    Update 04. Logical_Operators .sv

--- DAY_6/OPERATORS/04. Logical_Operators .sv
+++ DAY_6/OPERATORS/04. Logical_Operators .sv
@@ -1,1 +1,15 @@
-
+module operator;
+  logic [7:0] a,b,x,y,z;
+  initial begin
+    a = 4'b1010;
+    b = 4'b1000;
+    $display("Value of a %0b",a);
+    $display("Value of b %0b",b);
+    x = a && b;
+    $display("Value of logical and operator %0b",x);
+    y = a || b;
+    $display("Value of logical or operator %0b",y);
+    z = !b;
+    $display("Value of logical negation %0b",z);
+  end
+endmodule
