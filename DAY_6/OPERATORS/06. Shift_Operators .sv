625ee1e032cf7a78879f1b1f2e3af75d889b731c
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:19:23 +0530

    Add shift operators module with display examples

--- DAY_6/OPERATORS/06. Shift_Operators .sv
+++ DAY_6/OPERATORS/06. Shift_Operators .sv
@@ -1,1 +1,16 @@
+module operators;
+  logic [3:0] a, b;
+  initial begin
+    a = 4'b1011;
+    b = 4'b1111;
 
+    $display("Value of a = %b", a);
+    $display("Value of b = %b", b);
+	//Logical shift
+    $display("Logical left shift (a << 1)  = %b", a << 1);
+    $display("Logical right shift (a >> 1) = %b", a >> 1);
+	//Arithmetic shift
+    $display("Arithmetic left shift (a <<< 1)  = %b", a <<< 1);
+    $display("Arithmetic right shift (a >>> 1) = %b", a >>> 1);
+  end
+endmodule
