245eb11f4a57ea0bb94fd594fd410ab91aa1fad5
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:23:36 +0530

    Update 01. Arithmetic_Operators .sv

--- DAY_6/OPERATORS/01. Arithmetic_Operators .sv
+++ DAY_6/OPERATORS/01. Arithmetic_Operators .sv
@@ -1,1 +1,11 @@
-
+module operators;
+  int a = 22;
+  int b = 2;
+  initial begin
+    $display("a+b = %0d", a+b);
+    $display("a-b = %0d", a-b);
+    $display("a*b = %0d", a*b);
+    $display("a/b = %0d", a/b);
+    $display("a%%b = %0d", a%b);
+  end
+endmodule
