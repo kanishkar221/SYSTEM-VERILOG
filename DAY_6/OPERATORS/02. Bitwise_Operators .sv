98b0c13df5a82309f4c6dcb9a05e1a42d6618545
KANISHKA R <kanishka.r221@gmail.com>
2025-11-14 22:22:56 +0530

    Add Bitwise Operators module with display outputs

--- DAY_6/OPERATORS/02. Bitwise_Operators .sv
+++ DAY_6/OPERATORS/02. Bitwise_Operators .sv
@@ -1,1 +1,10 @@
-
+module operator;
+  bit [3:0] a = 4'b1011;
+  bit [3:0] b = 4'b0101;
+  initial begin
+    $display("a & b = %b", a & b);
+    $display("a | b = %b", a | b);
+    $display("a ^ b = %b", a ^ b);
+    $display("~a = %b", ~a);
+  end
+endmodule
