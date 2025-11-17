3068df448eac234320d497cd3285756ba7bff6f9
KANISHKA R <kanishka.r221@gmail.com>
2025-11-17 22:08:12 +0530

    Update 08. Inside_Operators .sv

--- DAY_6/OPERATORS/08. Inside_Operators .sv
+++ DAY_6/OPERATORS/08. Inside_Operators .sv
@@ -1,1 +1,13 @@
-
+module operator;
+  bit[3:0] bus_data;
+  initial begin
+    for(int i=0;i<4;i++)
+      begin
+        bus_data =$urandom;
+        if(bus_data inside {1,2,[5:8],10})
+          $display("bus_data=%0d 'inside' {1,2,[5:8],10}",bus_data);
+        else
+          $display("bus_data=%0d 'outside' {1,2,[5:8],10}",bus_data);
+      end
+  end
+endmodule
