fa18bd51ae86de1f81c1bce97690a94d458993e3
KANISHKA R <kanishka.r221@gmail.com>
2025-11-17 21:56:06 +0530

    Update 09. Wild_equality_non-equality_operators .sv

--- DAY_6/OPERATORS/09. Wild_equality_non-equality_operators .sv
+++ DAY_6/OPERATORS/09. Wild_equality_non-equality_operators .sv
@@ -1,1 +1,18 @@
+module operators;
+  logic [7:0] data;
+  initial begin
+    data = 8'hFF;
+    
+    if (data ==? 8'b11xx_z1xx)     // Wild equality
+      $display("Data matches %b", data);
+    else
+      $display("Data does not match %b", data);
+    
+    if (data !=? 8'b1xzx_z1x0)    // Wild non-equality
+      $display("Data does not match  %b", data);
+    else 
+      $display("Data matches with %b", data);
 
+    $finish;
+  end
+endmodule
