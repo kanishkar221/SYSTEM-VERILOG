b2983f2701263663c97f76bea17791707d10d38f
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:28:21 +0530

    Implement pass-by-value task in SystemVerilog

--- DAY_7/TASKS/02. Task - Pass_by_value .sv
+++ DAY_7/TASKS/02. Task - Pass_by_value .sv
@@ -1,1 +1,16 @@
+module task_pass_by_value;
+  int a, b;
+  int out;
 
+  task automatic add(int x, int y, output int result);
+    x = x + y;      
+    result = x;     
+  endtask
+  
+  initial begin
+    a = 10;
+    b = 20;
+    add(a, b, out); // pass-by-value (default)
+    $display("Out = %0d | a = %0d | b = %0d", out, a, b);
+  end
+endmodule
