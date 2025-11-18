cdd7fab650195583e871ed52fddb67e4ac7185b5
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:44:25 +0530

    Add automatic square function in SystemVerilog

--- DAY_7/FUNCTIONS/04. Function using automatic - square .sv
+++ DAY_7/FUNCTIONS/04. Function using automatic - square .sv
@@ -1,1 +1,11 @@
-
+module square_function;
+  function automatic int square(input int x);
+    return x*x;
+  endfunction
+  
+  initial begin
+    int r;
+    r = square(5);
+    $display("Square of the function = %0d",r);
+  end
+endmodule
