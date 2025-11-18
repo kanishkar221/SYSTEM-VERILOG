993aa1836124c4ed75c6d2fc7fc415048473113d
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:43:28 +0530

    Implement max_value function to find maximum of two integers

--- DAY_7/FUNCTIONS/03. Function - Maximum value .sv
+++ DAY_7/FUNCTIONS/03. Function - Maximum value .sv
@@ -1,1 +1,13 @@
-
+module function_example;
+  function int max_value(input int a, input int b);
+    if(a>b)
+      max_value = a;
+    else
+      max_value = b;
+  endfunction
+  initial begin
+    int r;
+    r = max_value(15,12);
+    $display("Maximum = %0d",r);
+  end
+endmodule
