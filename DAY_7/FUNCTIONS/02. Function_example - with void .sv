0ec042b824a1ec7208bb9973d46d130a2eaa6c4e
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:42:26 +0530

    Implement void function for addition example

--- DAY_7/FUNCTIONS/02. Function_example - with void .sv
+++ DAY_7/FUNCTIONS/02. Function_example - with void .sv
@@ -1,1 +1,13 @@
+module function_example;
+    function void add(input int a, input int b);
+      int sum;
+      sum = a + b;
+      $display("Addition = %0d", sum);
+    endfunction
 
+    initial begin
+        int result;
+      add(10, 20);  //Function call only, no return value
+      //result = add(10,20);  //ERROR — void functions return nothing
+    end
+endmodule
