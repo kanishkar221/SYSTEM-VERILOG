dadf96765092eb74b6101709e17a54d7326b70f5
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:45:40 +0530

    Implement function to add two integers by value

--- DAY_7/FUNCTIONS/05. Function - Pass by value .sv
+++ DAY_7/FUNCTIONS/05. Function - Pass by value .sv
@@ -1,1 +1,14 @@
-
+module function_pass_by_value;
+  int a, b;
+  int out;
+  function int add(int x, y);
+    x = x + y;
+    return x;
+  endfunction
+  initial begin
+    a = 10;
+    b = 20;
+    out = add(a, b);
+    $display("Out = %0d | a = %0d | b = %0d", out, a, b);
+  end
+endmodule
