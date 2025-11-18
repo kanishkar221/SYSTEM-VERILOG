f4a33346acc1dae798da5f3728cf54c893df944d
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:41:25 +0530

    Update 01. Function_example - without void .sv

--- DAY_7/FUNCTIONS/01. Function_example - without void .sv
+++ DAY_7/FUNCTIONS/01. Function_example - without void .sv
@@ -1,1 +1,10 @@
-
+module function_example;
+  function int add(input int a, b);
+    add = a + b;   // return value
+  endfunction
+  initial begin
+    int result;
+    result = add(10, 20);
+    $display("Addition = %0d", result);
+  end
+endmodule
