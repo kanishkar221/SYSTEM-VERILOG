a67cad06c0f0270d2a8a30e6dd1363dca50808bb
KANISHKA R <kanishka.r221@gmail.com>
2025-11-18 21:48:32 +0530

    Implement pass by reference function in SystemVerilog

--- DAY_7/FUNCTIONS/06. Function - Pass by reference .sv
+++ DAY_7/FUNCTIONS/06. Function - Pass by reference .sv
@@ -1,1 +1,16 @@
-
+module function_pass_by_reference;
+  int a,b;
+  int out;
+  
+  function automatic int add(ref int x, ref int y);
+    x = x + y; // update global variable
+    return x;
+  endfunction
+  
+  initial begin
+    a = 10;
+    b = 20;
+    out = add(a,b);
+    $display("Out = %0d | a = %0d | b = %0d",out,a,b); // a=30 is updated value
+  end
+endmodule
