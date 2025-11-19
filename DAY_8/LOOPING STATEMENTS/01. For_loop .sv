4c1ca80a34527466b44925b1c18c772e9e890a66
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:51:00 +0530

    Implement for loop to initialize and display array

--- DAY_8/LOOPING STATEMENTS/01. For_loop .sv
+++ DAY_8/LOOPING STATEMENTS/01. For_loop .sv
@@ -1,1 +1,12 @@
-
+module for_loop;
+  int array[10];
+  
+  initial begin
+    for (int i = 0; i < 10; i++) begin //Update array
+      array[i] = 10*i;
+    end
+    for (int i = 0; i < 10; i++) begin //Display array elements
+      $display("array[%0d] = %0d", i, array[i]);
+    end
+  end
+endmodule
