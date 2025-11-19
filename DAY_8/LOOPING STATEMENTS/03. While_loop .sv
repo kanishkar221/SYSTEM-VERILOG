dcbac76c2c238d84053555bfd4462a6c1db59ed7
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:56:29 +0530

    Update 03. While_loop .sv

--- DAY_8/LOOPING STATEMENTS/03. While_loop .sv
+++ DAY_8/LOOPING STATEMENTS/03. While_loop .sv
@@ -1,1 +1,20 @@
+module while_loop;
+  logic [2:0] count;
+  logic [3:0] mem [7:0];
+  
+  initial begin
+    count = 4;
+    while(count)
+      begin
+        mem[count] = $urandom;
+        $display("mem[%0d] = %0h",count,mem[count]);
+        count--;
+      end
+  end
+endmodule
 
+OUTPUT:
+  mem[4] = 1
+  mem[3] = 5
+  mem[2] = 7
+  mem[1] = 0
