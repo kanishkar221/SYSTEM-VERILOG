1335bf7fbce9e790c43bf9d141b6cb042f357fc7
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:39:47 +0530

    Update 03. Priority_if - statement .sv

--- DAY_8/CONDITIONAL STATEMENT/03. Priority_if - statement .sv
+++ DAY_8/CONDITIONAL STATEMENT/03. Priority_if - statement .sv
@@ -1,1 +1,13 @@
-
+module priority_if_example;  
+  initial begin
+    int a, b;
+    a = 1;
+    b = 10;
+    priority if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else 
+      $display("a is equal to b");
+  end
+endmodule
