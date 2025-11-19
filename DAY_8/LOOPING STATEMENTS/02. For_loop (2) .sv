75d65e362dccb2fd331504b5eacbbda09666131c
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:54:47 +0530

    Update 02. For_loop (2) .sv

--- DAY_8/LOOPING STATEMENTS/02. For_loop (2) .sv
+++ DAY_8/LOOPING STATEMENTS/02. For_loop (2) .sv
@@ -1,1 +1,15 @@
-
+module for_loop;
+  int i;
+  int mem[3:0];
+  initial begin
+    outerloop:for (int j=0 ; j<2; j++)
+      begin
+        #10;
+        innerloop:for (int i=0; i<3 ; i++)
+          begin
+            mem[i]=i+1;
+            $display("mem[%0d]=%0d",i,mem[i]);
+          end
+      end
+  end
+endmodule
