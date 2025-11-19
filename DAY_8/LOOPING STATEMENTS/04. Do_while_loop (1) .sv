584ab2fd59edf84cdf098550ff6eac7528283850
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:57:38 +0530

    Add do-while loop to display count
    Implement a do-while loop to display count values.

--- DAY_8/LOOPING STATEMENTS/04. Do_while_loop (1) .sv
+++ DAY_8/LOOPING STATEMENTS/04. Do_while_loop (1) .sv
@@ -1,1 +1,17 @@
+module do_while_loop;
+  int count;
+  initial begin
+    do begin
+      $display("Value of count = %0d",count);
+      count++;
+    end
+    while (count<5);
+  end
+endmodule
 
+OUTPUT:
+  Value of count = 0
+  Value of count = 1
+  Value of count = 2
+  Value of count = 3
+  Value of count = 4
