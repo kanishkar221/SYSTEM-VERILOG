8eed80b2c7c8783a90f793f908604df2284ed2a5
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:37:22 +0530

    Add if-else statement example in if_statement.sv

--- DAY_8/CONDITIONAL STATEMENT/01. if_statement .sv
+++ DAY_8/CONDITIONAL STATEMENT/01. if_statement .sv
@@ -1,1 +1,13 @@
-
+module if_example;  
+  initial begin
+    int a, b;
+    a = 20;
+    b = 20;
+    if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else 
+      $display("a is equal to b");
+  end
+endmodule
