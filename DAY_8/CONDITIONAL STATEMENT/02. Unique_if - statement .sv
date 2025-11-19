cfd29630d4368a066864ece27d8689a6f1358907
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:38:16 +0530

    Add unique if statement example in SystemVerilog

--- DAY_8/CONDITIONAL STATEMENT/02. Unique_if - statement .sv
+++ DAY_8/CONDITIONAL STATEMENT/02. Unique_if - statement .sv
@@ -1,1 +1,13 @@
-
+module unique_if_example;  
+  initial begin
+    int a, b;
+    a = 30;
+    b = 20;
+    unique if(a>b)
+      $display("a is greater than b");
+    else if(a<b)
+      $display("a is less than b");
+    else 
+      $display("a is equal to b");
+  end
+endmodule
