f46d25c6f68ea84484b2d18e04804b9d62786943
KANISHKA R <kanishka.r221@gmail.com>
2025-11-19 22:59:41 +0530

    Update 05. Do_while_loop (2) .sv

--- DAY_8/LOOPING STATEMENTS/05. Do_while_loop (2) .sv
+++ DAY_8/LOOPING STATEMENTS/05. Do_while_loop (2) .sv
@@ -1,1 +1,29 @@
+module do_while_loop;
+  int count;
+  initial begin
+    $display("Value of count outside the loop = %0d",count);
+    while(count<0)
+      begin
+        count++;
+        $display("Value of count inside the loop = %0d",count);
+      end
+  end
+endmodule
+
+// OUTPUT:Value of count outside the loop = 0
+
+module do_while_loop;
+  int count;
+  initial begin
+    $display("Value of count outside the loop = %0d",count);
+    do begin
+      $display("Value of count inside the loop = %0d",count);
+      count++;
+    end
+    while(count<0);
+  end
+endmodule
+
+//OUTPUT : Value of count outside the loop = 0
+//OUTPUT : Value of count inside the loop = 0
 
