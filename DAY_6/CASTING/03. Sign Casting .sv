e947156af564fd18d4580199090889751a0d6383
KANISHKA R <kanishka.r221@gmail.com>
2025-11-20 22:18:24 +0530

    Update 03. Sign Casting .sv

--- DAY_6/CASTING/03. Sign Casting .sv
+++ DAY_6/CASTING/03. Sign Casting .sv
@@ -1,1 +1,17 @@
+module sign_casting;
+  int a;
+  bit [31:0] b;
 
+  initial begin
+    a = 255;            //signed int=255
+    b = unsigned'(a);   //cast to unsigned 32 bit -m remains 255
+    $display("a=%0d b=%0d", a, b);
+    b = 47;             // 32 bit signed of 47       
+    a = signed'(b);     // b s signed int (47)
+    $display("a=%0d b=%0d", a, b); 
+  end
+endmodule
+
+OUTPUT
+    a=255 b=255
+    a=47 b=47
