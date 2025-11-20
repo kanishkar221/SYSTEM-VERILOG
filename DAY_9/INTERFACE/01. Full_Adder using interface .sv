8a07a743c27301f56ae93db6265b242bed0cdfbf
KANISHKA R <kanishka.r221@gmail.com>
2025-11-20 22:23:41 +0530

    Add full adder module with interface
    Implemented a full adder using half adders and an interface.

--- DAY_9/INTERFACE/01. Full_Adder using interface .sv
+++ DAY_9/INTERFACE/01. Full_Adder using interface .sv
@@ -1,1 +1,35 @@
+//Design Code
+module half_addr(input a, b, output so, co);
+  assign so = a ^ b;
+  assign co = a & b;
+endmodule
+
+module full_adder(fa_if inf);
+  wire s0, c0, c1;
+  half_addr HA1 (inf.a, inf.b, s0, c0);
+  half_addr HA2 (s0, inf.c, inf.s_out, c1);
+  
+  assign inf.c_out = c0 | c1;
+endmodule
+
+//Interface
+interface fa_if;          
+  logic a, b, c;
+  logic s_out, c_out;
+endinterface
+
+//Top Module
+module tb_top;           
+  fa_if inf();
+  full_adder fa(inf);
+  
+  initial begin
+    $monitor("a=%b b=%b c=%b,sum=%b,carry=%b",inf.a,inf.b,inf.c,inf.s_out,inf.c_out);
+    inf.a = 1; inf.b = 0; inf.c = 0;
+    #1;
+    inf.a = 1; inf.b = 1; inf.c = 1;
+    #1;
+    inf.a = 0; inf.b = 1; inf.c = 1;
+  end
+endmodule
 
