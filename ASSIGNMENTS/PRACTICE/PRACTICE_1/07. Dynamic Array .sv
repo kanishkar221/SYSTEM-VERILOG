518f391c54624fd1d8164572bb8250e2eb6c1b55
KANISHKA R <kanishka.r221@gmail.com>
2025-11-25 22:59:35 +0530

    Update 07. Dynamic Array .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_1/07. Dynamic Array .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_1/07. Dynamic Array .sv
@@ -1,1 +1,15 @@
-
+module dynamic_array;
+  string a[];
+  initial begin
+    $display("Size of a is %0d",a.size());
+    a = new[5];
+    a = {"Mango","Apple","Watermelon","Orange","Amla"};
+    $display("a = %0p",a);
+    a = new[15](a);
+    $display("a = %0p",a);
+    $display("Size of a is %0d",a.size());
+    a.delete();
+    $display("Size of a is %0d",a.size());
+  end
+endmodule
+ 
