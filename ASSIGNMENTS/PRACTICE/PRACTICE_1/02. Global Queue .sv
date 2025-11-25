28882827e8518f191603c0f98cd67b24ce1555ca
KANISHKA R <kanishka.r221@gmail.com>
2025-11-25 22:24:15 +0530

    Implement queue module with push and remove tasks

--- ASSIGNMENTS/PRACTICE/PRACTICE_1/02. Global Queue .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_1/02. Global Queue .sv
@@ -1,1 +1,26 @@
-
+module queue;  
+  int a[$];
+  task push_data;
+    begin
+      a.push_front(28);
+      a.push_back(16);
+    end
+  endtask
+  
+  task remove_data;
+    begin
+      a.pop_front();
+      a.pop_back();
+    end
+  endtask
+  
+  initial begin
+    a = '{12,28,5,16,18};
+    $display("A is :%0p",a);
+    push_data;
+    $display("A is :%0p",a);
+    remove_data;
+    $display("A is :%0p",a);
+  end
+endmodule
+   
