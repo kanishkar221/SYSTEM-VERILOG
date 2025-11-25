0cb90530989183646b1415c8a75f9f07710411b9
KANISHKA R <kanishka.r221@gmail.com>
2025-11-25 22:27:08 +0530

    Add union example module with packed types

--- ASSIGNMENTS/PRACTICE/PRACTICE_1/04. Union_example .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_1/04. Union_example .sv
@@ -1,1 +1,16 @@
-
+module union_example;
+  typedef union packed {
+    logic[15:0]word;
+    struct packed {
+      logic[7:0]byte1;
+      logic[7:0]byte2;
+    }bytes;
+  }word_t;
+  word_t my_word;
+  
+  initial begin
+    my_word.word = 16'hA5A5;   //A5=1010_0101
+    $display("Byte1:%b,Byte2:%h",my_word.bytes.byte1,my_word.bytes.byte2);
+  end
+endmodule
+  
