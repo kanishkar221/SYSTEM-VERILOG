ff2957b4e7d00f4b743f78705cdd8892a779dbc9
KANISHKA R <kanishka.r221@gmail.com>
2025-11-25 22:26:04 +0530

    Update 03. Function using sum of  N numbers .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_1/03. Function using sum of  N numbers .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_1/03. Function using sum of  N numbers .sv
@@ -1,1 +1,15 @@
-
+module test_sum_N_numbers;
+  int data[] = '{28,16,12,5,1,2};
+  int result;
+  function int sum_N_numbers(int ip[]);
+    int sum = 0;
+    foreach(ip[i])
+      sum=sum+ip[i];
+    return sum;
+  endfunction
+  
+  initial begin
+    result = sum_N_numbers(data);
+    $display("Sum of N numbers is %0d",result);
+  end
+endmodule
