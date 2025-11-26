eab6954e6bc3116e85622f9f6540a1762f922849
KANISHKA R <kanishka.r221@gmail.com>
2025-11-26 23:56:34 +0530

    Update 03. Generate Specific Value .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/03. Generate Specific Value .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/03. Generate Specific Value .sv
@@ -1,1 +1,21 @@
+//Write a constraint to generate specific values: 25, 27, 30, 36, 40, 45.
+
+class packet;
+  rand int value;
+  constraint specific_values {
+    value inside {25, 27, 30, 36, 40, 45};
+  }
+endclass
+
+module tb;
+  packet pkt = new();
+  initial begin
+    repeat (10) begin
+      if (pkt.randomize())
+        $display("Generated value = %0d", pkt.value);
+      else
+        $display("Not Generated value");
+    end
+  end
+endmodule
 
