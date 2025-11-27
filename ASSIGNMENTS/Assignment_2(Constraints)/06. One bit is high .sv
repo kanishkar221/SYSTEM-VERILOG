ff308f1ad089d8127d666629012990b19f6c51ed
KANISHKA R <kanishka.r221@gmail.com>
2025-11-27 21:36:02 +0530

    Update 06. One bit is high .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/06. One bit is high .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/06. One bit is high .sv
@@ -1,1 +1,22 @@
+// 6.Write a constraint to generate a 32-bit number where exactly one bit is high.
 
+class packet;
+  randc bit [31:0] data;
+
+  // Constraint: exactly one bit must be high
+  constraint one_high_bit {
+    $countones(data) == 1;
+  }
+endclass
+
+
+module tb;
+  packet pkt = new();
+  
+  initial begin
+    repeat(10) begin
+      pkt.randomize();
+      $display("Generated one high bit data = %b", pkt.data);
+    end
+  end
+endmodule
