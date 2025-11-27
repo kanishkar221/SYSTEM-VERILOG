d65f94c22e078060314dd6984e4f51e3872c1b88
KANISHKA R <kanishka.r221@gmail.com>
2025-11-27 07:47:26 +0530

    Update 01. 12_non-consecutive_ones .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/01. 12_non-consecutive_ones .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/01. 12_non-consecutive_ones .sv
@@ -1,1 +1,32 @@
+//Write a constraint for a 32-bit variable such that exactly 12 non-consecutive 1's are generated.
+
+class nonconsecutive_12_ones;
+  rand bit [31:0] data;
+
+  constraint non_consecutive_c {
+    $countones(data) == 12;    // Exactly 12 ones
+	foreach (data[i])          // No two consecutive ones
+      if (i < 31)
+        (data[i] + data[i+1]) <= 1;
+  }
+
+  function void display();
+    $display("Data = %032b  || ones = %0d", data, $countones(data));
+  endfunction
+endclass
+
+module tb;
+  nonconsecutive_12_ones obj;
+
+  initial begin
+    obj = new();
+    repeat (15) begin
+      if (!obj.randomize())
+        $display("Randomization failed!");
+      else
+        obj.display();
+    end
+    $finish;
+  end
+endmodule
 
