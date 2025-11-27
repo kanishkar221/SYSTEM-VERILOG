c3a16e255bb169a0c8b670669526e21c49eab739
KANISHKA R <kanishka.r221@gmail.com>
2025-11-27 21:43:06 +0530

    Update 08. Even number in odd positions .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/08. Even number in odd positions .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/08. Even number in odd positions .sv
@@ -1,1 +1,24 @@
+// 8. Write a constraint to generate a sequence where even numbers appear in odd positions.
 
+class packet;
+  rand bit[3:0]seq[10];
+  
+  constraint position {
+    foreach (seq[i]) {
+      if (i % 2 == 1)        // Odd position (1,3,5,7,...)
+        seq[i] % 2 == 0;     // Value must be in even
+      else                   // Even position (2,4,6,8,...)
+        seq[i] % 2 == 1;     // Value must be in odd
+    }
+  }
+endclass
+      
+module tb;
+  packet pkt = new();
+  initial begin
+    pkt.randomize();
+    $display("Generated Sequence:");
+    foreach (pkt.seq[i])
+      $display("Position %0d = %0d",i,pkt.seq[i]);
+  end
+endmodule
