191de3f5e952c0ed2ed9b48968a878858bc06ef6
KANISHKA R <kanishka.r221@gmail.com>
2025-11-27 21:44:30 +0530

    Update 09. Unique multiple of 3 .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/09. Unique multiple of 3 .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/09. Unique multiple of 3 .sv
@@ -1,1 +1,25 @@
+// 9.Write a constraint to generate unique multiples of 3.
 
+class packet;
+  rand bit[3:0] data;
+  bit [3:0] values[$];        // Dynamic array to store values
+  
+  constraint unique_mul_3 {
+    data % 3 == 0;            // Multiple of 3
+    foreach (values[i])
+      data != values[i];      // Values must not repeat
+  }
+endclass
+
+module tb;
+  packet pkt = new();
+  initial begin
+    repeat (6) begin
+      if(pkt.randomize()) begin
+        $display("Unique multiple of 3 = %0d",pkt.data);
+        pkt.values.push_back(pkt.data);
+      end
+    end
+  end
+endmodule
+      
