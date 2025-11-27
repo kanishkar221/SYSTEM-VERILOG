0ffd837ecf2a39251b39a108337bf441dd5cbf5a
KANISHKA R <kanishka.r221@gmail.com>
2025-11-27 08:04:58 +0530

    Update 05. No two consecutive ones .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/05. No two consecutive ones .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/05. No two consecutive ones .sv
@@ -1,1 +1,23 @@
+// Write a constraint for a 16-bit variable such that no two consecutive 1's are generated.
 
+class packet;
+  rand bit [15:0] data;
+
+  // Constraint: no consecutive 1's
+  constraint no_consecutive {
+    foreach (data[i])
+      if (i < 15)
+        !(data[i] && data[i+1]);   // adjacent 1's not allowed
+  }
+endclass
+
+
+module tb;
+  packet pkt = new();
+  initial begin
+    repeat (10) begin
+      pkt.randomize();
+      $display("Generated data = %016b", pkt.data);
+    end
+  end
+endmodule
