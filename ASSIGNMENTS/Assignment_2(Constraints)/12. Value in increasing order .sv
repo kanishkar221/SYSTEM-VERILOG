e33bf4642133b7458510a60b32d7cb59a52f63c5
KANISHKA R <kanishka.r221@gmail.com>
2025-11-30 19:07:50 +0530

    Update 12. Value in increasing order .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/12. Value in increasing order .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/12. Value in increasing order .sv
@@ -1,1 +1,20 @@
+// 12. Write a constraint to generate values in increasing order.
 
+class packet;
+  rand bit [3:0] val[15];
+  constraint c{
+    foreach (val[i])
+      if(i<14)
+        val[i]<val[i+1];
+  }
+endclass
+
+module tb;
+  packet pkt = new();
+  
+  initial begin
+    pkt.randomize();
+    foreach (pkt.val[i])
+      $display("Generate values in increasing order = %d",pkt.val[i]);
+  end
+endmodule
