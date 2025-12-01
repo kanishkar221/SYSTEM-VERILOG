34302ececfea89c2dd1c58ee143db9afc222f564
KANISHKA R <kanishka.r221@gmail.com>
2025-12-01 22:26:07 +0530

    Update 05. Number .sv

--- ASSIGNMENTS/SV Assignment_1/05. Number .sv
+++ ASSIGNMENTS/SV Assignment_1/05. Number .sv
@@ -1,1 +1,18 @@
+// 5. Predict the output for the below code
 
+module top;
+  int num1;
+  int num2;
+  initial begin
+    num1 = int '(10.0 - 1.8);
+    $display("Integer value is : %d",num1);
+    num2 = int '(5/3);
+    $display("Integer value is : %d",num2);
+  end
+endmodule
+
+
+OUTPUT
+	Integer value is : 8
+	Integer value is : 1
+	
