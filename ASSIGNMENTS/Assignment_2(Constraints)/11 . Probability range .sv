cd157b2d18e665fd657015465f6c76fa73e4a13d
KANISHKA R <kanishka.r221@gmail.com>
2025-12-01 09:33:40 +0530

    Update 11 . Probability range .sv

--- ASSIGNMENTS/Assignment_2(Constraints)/11 . Probability range .sv
+++ ASSIGNMENTS/Assignment_2(Constraints)/11 . Probability range .sv
@@ -1,1 +1,22 @@
+// 11.Write a constraint where a variable's range of 0-100 has 10% probability and 101-255 has 30% probability.
 
+class packet;
+  rand bit [7:0] data;
+
+  constraint c1 {
+    data dist {
+      [0  :100] := 10,
+      [101:255] := 30
+    };
+  }
+endclass
+
+module tb;
+  initial begin
+    packet pkt = new();
+    repeat (5) begin
+      pkt.randomize();
+      $display("Generated data = %0d", pkt.data);
+    end
+  end
+endmodule
