ee0b1d5282e9a4a74d77167e96a6b3ffbf040028
KANISHKA R <kanishka.r221@gmail.com>
2025-12-03 21:39:42 +0530

    Update 02. Class Example (2) .sv

--- DAY_10/CLASS/02. Class Example (2) .sv
+++ DAY_10/CLASS/02. Class Example (2) .sv
@@ -1,1 +1,30 @@
+// CODE 
+class home;
+   bit light;    
+   int fan;
+   string switch;
 
+   task open_electricity();
+     switch = "ON";
+     $display("Switch is %s, so electricity is open", switch);
+   endtask
+endclass
+
+// TESTBENCH
+
+module check_electricity;
+  initial begin
+    home h1;
+    h1 = new();
+    h1.light = 1;
+    h1.fan = 1;
+    h1.open_electricity;
+    $display("Light is on = %0d",h1.light);
+    $display("Fan is on = %0d",h1.fan);
+  end
+endmodule
+
+// OUTPUT 
+Switch is ON, so electricity is open
+Light is on = 1
+Fan is on = 1
