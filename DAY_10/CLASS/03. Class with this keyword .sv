40678f360273d597c03f2984dd56ba4d6e7fcf80
KANISHKA R <kanishka.r221@gmail.com>
2025-12-03 21:40:59 +0530

    Update 03. Class with this keyword .sv

--- DAY_10/CLASS/03. Class with this keyword .sv
+++ DAY_10/CLASS/03. Class with this keyword .sv
@@ -1,1 +1,26 @@
+class base_class;           // Using this keyword
+  string fan = "OFF";
+  string switch = "OFF";
+  
+  function void open_electricity();
+    string fan = "ON";
+    string switch = "ON";
+    this.fan = fan;
+    this.switch = switch;
+    $display("Inside class: Switch is %s ,so fan is %s",switch,fan);
+  endfunction
+endclass
 
+module check_electricity;
+  base_class b1;
+  initial begin
+    b1 = new();
+    b1.open_electricity;
+    $display("Outside class: Switch is %s ,so fan is %s",b1.switch,b1.fan);
+  end
+endmodule
+
+
+// OUTPUT
+	Inside class: Switch is ON ,so fan is ON
+	Outside class: Switch is ON ,so fan is ON
