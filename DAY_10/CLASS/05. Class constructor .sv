a83970de34650b8982f9db724190202e7e5853af
KANISHKA R <kanishka.r221@gmail.com>
2025-12-03 21:50:19 +0530

    Update 05. Class constructor .sv

--- DAY_10/CLASS/05. Class constructor .sv
+++ DAY_10/CLASS/05. Class constructor .sv
@@ -1,1 +1,40 @@
+class packet;
+  bit [31:0] addr;    // class properties
+  bit [31:0] data;
+  bit write;
+  string pkt_type;
+  
+  function new();     // constructor
+    this.addr = 32'h10;
+    this.data = 32'hFF;
+    this.write = 1;
+    this.pkt_type = "GOOD";
+  endfunction
+  
+  function void display();
+    $display("addr = %0d",addr);
+    $display("data = %0h",data);
+    $display("write = %0d",write);
+    $display("pkt_type = %0s",pkt_type);
+  endfunction
+endclass
+
+// TEST BENCH
+
+module sv_constructor;
+  packet pkt;
+  initial begin
+    pkt = new();
+    pkt.display();
+  end
+endmodule
+
+// OUTPUT
+
+addr = 16
+data = ff
+write = 1
+pkt_type = GOOD
+
+
 
