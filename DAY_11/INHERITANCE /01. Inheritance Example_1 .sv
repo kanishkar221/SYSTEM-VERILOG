0dec20652b0f8782a89943011454331b6f7b1176
KANISHKA R <kanishka.r221@gmail.com>
2025-12-04 22:30:45 +0530

    Update 01. Inheritance Example_1 .sv

--- DAY_11/INHERITANCE /01. Inheritance Example_1 .sv
+++ DAY_11/INHERITANCE /01. Inheritance Example_1 .sv
@@ -1,1 +1,19 @@
+class parent_class;
+  bit [31:0] addr = 5;
+endclass
 
+class child_class extends parent_class;
+  bit [31:0] data = 2;
+endclass
+
+module inheritance;
+  initial begin
+    child_class c = new();
+    //c.addr = 10;
+    c.data = 20;
+    $display("Value of addr = %0d data = %0d",c.addr,c.data);
+  end
+endmodule
+
+// OUTPUT 
+Value of addr = 5 data = 20
