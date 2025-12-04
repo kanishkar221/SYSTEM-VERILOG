4552f0bafba83ab266fdbba6b81a77c4b25497c3
KANISHKA R <kanishka.r221@gmail.com>
2025-12-04 22:32:51 +0530

    Update 03. Multilevel Inheritance .sv

--- DAY_11/INHERITANCE /03. Multilevel Inheritance .sv
+++ DAY_11/INHERITANCE /03. Multilevel Inheritance .sv
@@ -1,1 +1,57 @@
+class parent_class;
+  bit[31:0] data_p;
+  function void disp_p();
+    $display("Value of data_p = %0h",data_p);
+  endfunction
+endclass
 
+class child_1_class extends parent_class;
+  bit[31:0] data_c1;
+  function void disp_c1();
+    $display("Value of data_c1 = %0h",data_c1);
+  endfunction
+endclass
+
+class child_2_class extends parent_class;
+  bit[31:0] data_c2;
+  function void disp_c2();
+    $display("Value of data_c2 = %0h",data_c2);
+  endfunction
+endclass
+
+class child_A_class extends child_1_class;
+  bit[31:0] data_cA;
+  function void disp_cA();
+    $display("Value of data_cA = %0h",data_cA);
+  endfunction
+endclass
+
+class child_B_class extends child_A_class;
+  bit[31:0] data_cB;
+  function void disp_cB();
+    $display("Value of data_cB = %0h",data_cB);
+  endfunction
+endclass
+
+module class_example;
+  initial begin
+    child_B_class cB_class = new();
+    cB_class.data_p = 2;
+    cB_class.data_c1 = 4;
+    //cB_class.data_c2 = 5;
+    cB_class.data_cA = 6;
+    cB_class.data_cB = 8;
+    
+    cB_class.disp_p();
+    cB_class.disp_c1();
+    //cB_class.disp_c2();
+    cB_class.disp_cA();
+    cB_class.disp_cB();
+  end
+endmodule
+
+// OUTPUT
+Value of data_p = 2
+Value of data_c1 = 4
+Value of data_cA = 6
+Value of data_cB = 8
