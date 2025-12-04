376a7c257bd06bdae7da16128032549552d5877c
KANISHKA R <kanishka.r221@gmail.com>
2025-12-04 22:26:04 +0530

    Update Parameterized by type (datatype) .sv

--- DAY_11/Parameterized Classes/Parameterized by type (datatype) .sv
+++ DAY_11/Parameterized Classes/Parameterized by type (datatype) .sv
@@ -1,1 +1,27 @@
+class data #(parameter int a = 4,type team = string);
+  bit [a-1:0]d;
+  team c;
+  
+  function new();
+    d = 20;
+    c = "Company";
+  endfunction
+  
+  function void display();
+    $display ("d = %0d,c = %0s",d,c);
+  endfunction
+endclass
+
+module test;
+  data #(4) p1;
+  initial begin
+    p1 = new();
+    $display("Contents of p1");
+    p1.display();
+  end
+endmodule
+
+// OUTPUT
+Contents of p1
+d = 4,c = Company
 
