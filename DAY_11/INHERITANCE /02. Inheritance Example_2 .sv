1f95e499a7c2b23107dc1adbf63a06e0ef9d91a9
KANISHKA R <kanishka.r221@gmail.com>
2025-12-04 22:31:46 +0530

    Update 02. Inheritance Example_2 .sv

--- DAY_11/INHERITANCE /02. Inheritance Example_2 .sv
+++ DAY_11/INHERITANCE /02. Inheritance Example_2 .sv
@@ -1,1 +1,29 @@
+class parent_class;
+  bit [31:0] addr;
+  function void disp_p();
+    $display("Value of addr = %0h",addr);
+  endfunction
+endclass
 
+class child_class extends parent_class;
+  int id;
+  function void disp_c();
+    $display("Value of id = %0h",id);
+  endfunction
+endclass
+
+module class_example;
+  initial begin
+    child_class c = new();
+    c.addr = 5;
+    c.id = 1;
+    c.disp_p();
+    c.disp_c();
+  end
+endmodule
+
+// OUTPUT 
+Value of addr = 5
+Value of id = 1
+    
+  
