9ce1bdd799a34e36263fa6425325a75214c3ea3a
KANISHKA R <kanishka.r221@gmail.com>
2025-12-05 22:07:22 +0530

    Update 02. Example_2 .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_3 (OOP)/02. Example_2 .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_3 (OOP)/02. Example_2 .sv
@@ -1,1 +1,19 @@
+// class methods and constructor
+class packet;
+  rand bit [7:0] data = 5;
+  rand bit [15:0] addr = 2;
+  
+  function void display();
+    $display("Data = %0d,Addr = %0d",this.data,this.addr);
+  endfunction
+endclass
 
+module example;
+  packet pkt = new();  // create an instance of packet
+  initial begin
+    pkt.display();     // calls display method on this instance
+  end
+endmodule
+
+// OUTPUT
+Data = 5,Addr = 2
