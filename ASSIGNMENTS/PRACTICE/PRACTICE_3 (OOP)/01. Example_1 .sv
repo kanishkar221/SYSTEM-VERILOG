ce661492819898acfd92f3c12604fbcdbafb7612
KANISHKA R <kanishka.r221@gmail.com>
2025-12-05 22:04:11 +0530

    Update 01. Example_1 .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_3 (OOP)/01. Example_1 .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_3 (OOP)/01. Example_1 .sv
@@ -1,1 +1,20 @@
+// class and create a instance in SV
+class packet;
+  rand bit [7:0] data;
+  rand bit [7:0] addr;
+  
+  function void display();
+    $display("Data = %0d,Addr = %0d",data,addr);
+  endfunction
+endclass
 
+module example;
+  packet pkt = new(); // create an instance of packet
+  initial begin
+    pkt.randomize();
+    pkt.display();
+  end
+endmodule
+
+// OUTPUT
+Data = 55,Addr = 73
