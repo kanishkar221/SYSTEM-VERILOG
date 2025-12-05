e107914d2a2f38cf815d97c1f6d3941af2e2a1a5
KANISHKA R <kanishka.r221@gmail.com>
2025-12-05 22:21:49 +0530

    Update 01. Example_1 .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_4 (OOP)/01. Example_1 .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_4 (OOP)/01. Example_1 .sv
@@ -1,1 +1,24 @@
+class BaseClass;
+  bit [7:0] id;
+  function void display();
+    $display("Base Class ID = %0d",id);
+  endfunction
+endclass
 
+class DerivedClass extends BaseClass;
+  function void display();
+    super.display();
+    $display("Derived Class");
+  endfunction
+endclass
+
+module example;
+  DerivedClass obj = new();
+  initial begin
+    obj.id = 42;
+    obj.display();
+  end
+endmodule
+ 
+// OUTPUT 
+Base Class ID = 42
