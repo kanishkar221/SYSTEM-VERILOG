f4cdcdfa48262e7cc36b1c68513a1064dd4cadc8
KANISHKA R <kanishka.r221@gmail.com>
2025-12-05 21:45:12 +0530

    Update 02. Example_2 .sv

--- DAY_12/Super Keyword/02. Example_2 .sv
+++ DAY_12/Super Keyword/02. Example_2 .sv
@@ -1,1 +1,26 @@
+class parent_class;
+  string fan, switch;
+  function void display();
+    switch = "ON";
+    $display("Switch is %s",switch);
+  endfunction
+endclass
 
+class child_class extends parent_class;
+  string fan = "ON";
+  function void display();
+    super.display();                // Here using super keyword we can get both display() methods
+    $display("So fan is %s",fan);
+  endfunction
+endclass
+
+module class_example;
+  initial begin
+    child_class c1 = new();
+    c1.display();
+  end
+endmodule
+
+// OUTPUT 
+Switch is ON
+So fan is ON
