0c7c27fd7aee891fd8dddf90f098128e6809f2c9
KANISHKA R <kanishka.r221@gmail.com>
2025-12-05 22:25:58 +0530

    Update 04. Example_4 .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_4 (OOP)/04. Example_4 .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_4 (OOP)/04. Example_4 .sv
@@ -1,1 +1,26 @@
+class Vehicle;
+  string model_name;
+  function new (string model_name);
+    this.model_name = model_name;
+    $display("Vehicle Model = %s",model_name);
+  endfunction
+endclass
 
+class Car extends Vehicle;
+  string colour;
+  function new (string model_name,string colour);
+    super.new(model_name);
+    this.colour = colour;
+    $display("Car Colour = %s",colour);
+  endfunction
+endclass
+
+module example;
+  initial begin
+    Car c1 = new("BENZ","GREY");
+  end
+endmodule
+
+// OUTPUT
+Vehicle Model = BENZ
+Car Colour = GREY
