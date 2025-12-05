e59da0d7dd7c905800042cc0b62ff5cef883ce0a
KANISHKA R <kanishka.r221@gmail.com>
2025-12-05 22:23:15 +0530

    Update 02. Example_2 .sv

--- ASSIGNMENTS/PRACTICE/PRACTICE_4 (OOP)/02. Example_2 .sv
+++ ASSIGNMENTS/PRACTICE/PRACTICE_4 (OOP)/02. Example_2 .sv
@@ -1,1 +1,23 @@
+class ParentClass;
+  virtual function void display(input bit[7:0] data);
+    $display("Parent:data = %0h",data);
+  endfunction
+endclass
 
+class ChildClass extends ParentClass;
+  function void display(input bit[7:0] data);
+    $display("Child:data = %0h",data);
+    super.display(data);
+  endfunction
+endclass
+
+module example;
+  initial begin
+    ChildClass c1 = new();
+    c1.display(8'hA5);
+  end
+endmodule
+
+// OUTPUT 
+Child:data = a5
+Parent:data = a5
