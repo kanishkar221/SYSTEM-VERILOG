b6c791e4b00fd4452895b8b02d0996fd45d071f4
KANISHKA R <kanishka.r221@gmail.com>
2025-12-06 14:03:02 +0530

    Update 09. Array Locator Methods .sv

--- ASSIGNMENTS/SV Assignment_1/09. Array Locator Methods .sv
+++ ASSIGNMENTS/SV Assignment_1/09. Array Locator Methods .sv
@@ -17,56 +17,43 @@
 
 // CODE
 
-module array;
-  int q[$]='{1,3,5,7};
+module array_locator;
+  int q[$] = '{1,3,5,7};
   int tq[$];
-  int d[] = {9,1,8,3,4,4};
-  int f[6] = {1,6,2,6,8,6};
-  int count;
+  int d[] = '{9,1,8,3,4,4};
+  int f[6] = '{1,6,2,6,8,6};
   initial begin
-   // c. display sum, product of elements of array q
-    $display(q.sum());
-    $display(q.product());
-   //d. display min, max values stored in array q
-    $display(q.min());
-    $display(q.max());
-    //e. display the unique elements from array f
-    $display(f.unique());
-    //f. find elements in array d with condition item > 3
-    tq = d.find with(item>3);
-    $display(tq);
-    //g. find indexes of those elements in array d which have condition item > 3
-    tq = d.find_index with (item>3);
-    $display(tq);
-    //h. find indexes of those elements in array d which have condition item > 99
-    tq = d.find_index with (item>99);
-    $display(tq);
-    //i. find the first index in array d which matches with condition item==8
-    tq = d.find_index with (item==8);
-    $display(tq);
-    //j. find the last element in array d which matches with condition item==4
-    tq = d.find_last with(item==4);
-    $display(tq);
-    //k. find the last index in array d which matches with condition item==4
-    tq = d.find_last_index with (item==4); 
-    $display(tq);
-    //l. find the sum of elements in array d with condition item > 7
-    count = d.sum();
-    $display(count);
-    //m. find the sum of elements in array d with condition ((item > 7) * item)
-    count = d.sum with((item > 7) * item); 
-    $display(count);
-    //n. find the sum of elements in array d with condition item < 8
-    count = d.sum with(item < 8);
-    $display(count);
-    //o. find the sum of elements in array d with condition ((item < 8)? item:0)
-    count = d.sum with((item < 8)? item:0);
-    $display(count);
+    $display("sum of q:",q.sum());
+    $display("product of q:",q.product());
+    $display("min of q:",q.min());
+    $display("max of q:",q.max());
+    $display("unique of f=",f.unique());
+    $display("item>3 elements of d =",d.find with(item>3));
+    $display("item>3 indices of d =",d.find_index with(item>3));
+    $display("item>99 indices of d =",d.find_index with(item>99));
+    $display("First index(item==8) of d =",d.find_first_index with(item ==8));
+    $display("Last element (item==4) of d =",d.find_last with(item ==4));
+    $display("Last index (item==4) of d =",d.find_last_index with(item ==4));
+    $display("Sum of elements with (item>7) of d =",d.sum with((item>7)*32'd1));
+    $display("Sum of elements with (item>7)*item of d =",d.sum with((item >7)*item));
+    $display("Sum of elements with (item<8) of d =",d.sum with(item <8));  
+    $display("Sum of elements with (item<8)?item:0 of d =",d.sum with((item <8)?item:0));
   end
 endmodule
 
 // OUTPUT 
-
-
-
-
+sum of q: 16
+product of q: 105
+min of q:'{1}
+max of q:'{7}
+unique of f='{1, 2, 6, 8}
+item>3 elements of d ='{9, 8, 4, 4}
+item>3 indices of d ='{0, 2, 4, 5}
+item>99 indices of d ='{}
+First index(item==8) of d ='{2}
+Last element (item==4) of d ='{4}
+Last index (item==4) of d ='{5}
+Sum of elements with (item>7) of d = 2
+Sum of elements with (item>7)*item of d = 17
+Sum of elements with (item<8) of d = 0
+Sum of elements with (item<8)?item:0 of d = 12
