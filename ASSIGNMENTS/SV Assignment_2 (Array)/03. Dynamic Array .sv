098a4fa625f84a5befee5409b3550be52cedc387
KANISHKA R <kanishka.r221@gmail.com>
2025-12-07 22:22:35 +0530

    Update 03. Dynamic Array .sv

--- ASSIGNMENTS/SV Assignment_2 (Array)/03. Dynamic Array .sv
+++ ASSIGNMENTS/SV Assignment_2 (Array)/03. Dynamic Array .sv
@@ -1,1 +1,22 @@
+3. write a code for dynamic array... give the value of array using foreach.. display the size.. shuffle the array elements...
 
+// CODE
+module dynamic_array;
+  int arr[];
+  initial begin
+    arr = new [5];
+    foreach(arr[i]) begin
+      arr[i] = (i+1)*10;
+    end
+
+    $display("Size of the array = %0d",arr.size());
+    $display("Array before Shuffle = %p",arr);
+    arr.shuffle();
+    $display("After after Shuffle = %p",arr);
+  end
+endmodule
+
+// OUTPUT
+Size of the array = 5
+Array before Shuffle = '{10, 20, 30, 40, 50}
+After after Shuffle = '{50, 20, 10, 30, 40}
