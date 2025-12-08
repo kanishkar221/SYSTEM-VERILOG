f2f01f534b0ca9025f41127da05274be46083267
KANISHKA R <kanishka.r221@gmail.com>
2025-12-08 21:49:23 +0530

    Update 01. Tasks with virtual keyword .sv

--- DAY_12/Virtual Methods /Virtual Tasks /01. Tasks with virtual keyword .sv
+++ DAY_12/Virtual Methods /Virtual Tasks /01. Tasks with virtual keyword .sv
@@ -1,1 +1,37 @@
+class packet;  // with virtual keyword
+  string a;
+  int b;
+  virtual task display();
+    a = "Team";
+    b = 4;
+    $display("a = %s",a);
+    $display("b = %0d",b);
+  endtask
+endclass
 
+class pack extends packet;
+  string c;
+  int d;
+  task display();
+    c = "RKS";
+    d = 5;
+    $display("c = %s",c);
+    $display("d = %0d",d);
+  endtask
+endclass
+
+module virtual_task;
+  packet p1;
+  pack p2;
+  initial begin
+    p2 = new();
+    p1 = p2;
+    $display("Contents of p1");
+    p1.display();
+  end
+endmodule    
+
+// OUTPUT
+Contents of p1
+c = RKS
+d = 5
