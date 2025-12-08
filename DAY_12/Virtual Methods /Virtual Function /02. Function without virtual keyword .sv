157d66e10373a9871bd7c747f37919b2f5aefffa
KANISHKA R <kanishka.r221@gmail.com>
2025-12-08 21:45:14 +0530

    Update 02. Function without virtual keyword .sv

--- DAY_12/Virtual Methods /Virtual Function /02. Function without virtual keyword .sv
+++ DAY_12/Virtual Methods /Virtual Function /02. Function without virtual keyword .sv
@@ -1,1 +1,24 @@
+class base_class;    // method without virtual keyword
+  function void display();
+    $display("Inside Base_Class");
+  endfunction
+endclass
 
+class extended_class extends base_class;
+  function void display();
+    $display("Inside Extended_Class");
+  endfunction
+endclass
+
+module virtual_example;
+  initial begin
+    base_class b_c;
+    extended_class e_c;
+    e_c = new();
+    b_c = e_c;
+    b_c.display();
+  end
+endmodule
+
+// OUTPUT
+Inside Base_Class
