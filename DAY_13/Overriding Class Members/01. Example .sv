2bee734199412032aca1a3e3cf7604dcb128af71
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 21:44:10 +0530

    Update 01. Example .sv

--- DAY_13/Overriding Class Members/01. Example .sv
+++ DAY_13/Overriding Class Members/01. Example .sv
@@ -1,1 +1,25 @@
+class parent_class;
+  bit [31:0] addr;
+  function display();
+    $display("Addr = %0d",addr);
+  endfunction
+endclass
 
+class child_class extends parent_class;
+  bit [31:0] data;;
+  function display();
+    $display("Data = %0d",data);
+  endfunction
+endclass
+
+module example;
+  child_class c = new();
+  initial begin
+    c.addr = 10;
+    c.data = 20;
+    c.display();
+  end
+endmodule
+
+// OUTPUT
+Data = 20
