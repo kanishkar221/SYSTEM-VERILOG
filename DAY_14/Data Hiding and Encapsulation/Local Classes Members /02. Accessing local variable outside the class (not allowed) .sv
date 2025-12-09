ce87969d1d3cd0fd4bec68fa60ae25f91745a7e4
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 22:17:02 +0530

    Update 02. Accessing local variable outside the class (not allowed) .sv

--- DAY_14/Data Hiding and Encapsulation/Local Classes Members /02. Accessing local variable outside the class (not allowed) .sv
+++ DAY_14/Data Hiding and Encapsulation/Local Classes Members /02. Accessing local variable outside the class (not allowed) .sv
@@ -1,1 +1,20 @@
+// Accessing local variable outside the class (not allowed)
+class parent_class;
+  local bit[31:0] tmp_addr;
+  function new(bit[31:0] r_addr);
+    tmp_addr = r_addr + 10;
+  endfunction
+  
+  function display();
+    $display("tmp_addr = %0d",tmp_addr);
+  endfunction
+endclass
 
+module example;
+  initial begin
+    parent_class p_c = new();
+    p_c.tmp_addr = 20; // Accessing local variable outside the class
+    p_c.display();
+  end
+endmodule
+    
