bd33366914b0524314f4a3bea631e9005d0754f9
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 22:16:00 +0530

    Update 01. Accessing local variable within the class (allowed) .sv

--- DAY_14/Data Hiding and Encapsulation/Local Classes Members /01. Accessing local variable within the class (allowed) .sv
+++ DAY_14/Data Hiding and Encapsulation/Local Classes Members /01. Accessing local variable within the class (allowed) .sv
@@ -1,1 +1,21 @@
+// Accessing local variable within the class (allowed)
+class parent_class;
+  local bit[31:0] tmp_addr;
+  function new(bit[31:0] r_addr);
+    tmp_addr = r_addr + 10;
+  endfunction
+  
+  function display();
+    $display("tmp_addr = %0d",tmp_addr);
+  endfunction
+endclass
 
+module example;
+  initial begin
+    parent_class p_c = new(5);
+    p_c.display();
+  end
+endmodule
+
+// OUTPUT
+tmp_addr = 15
