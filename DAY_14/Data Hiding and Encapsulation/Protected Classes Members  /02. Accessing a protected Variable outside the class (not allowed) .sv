03851f4cab40095ee4e817d9ab37e459514308ed
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 22:23:41 +0530

    Update 02. Accessing a protected Variable outside the class (not allowed) .sv

--- DAY_14/Data Hiding and Encapsulation/Protected Classes Members  /02. Accessing a protected Variable outside the class (not allowed) .sv
+++ DAY_14/Data Hiding and Encapsulation/Protected Classes Members  /02. Accessing a protected Variable outside the class (not allowed) .sv
@@ -1,1 +1,33 @@
+// Accessing a protected Variable outside the class (not allowed)
+class parent_class;
+  protected bit [31:0] tmp_addr;
+  function new(bit[31:0]r_addr);
+    tmp_addr = r_addr+10;
+  endfunction
+  
+  function display();
+    $display("tmp_addr = %0d",tmp_addr);
+  endfunction
+endclass
 
+class child_class extends parent_class;
+  function new(bit[31:0]r_addr);
+    super.new(r_addr);
+  endfunction
+  
+  function void incr_addr();
+    tmp_addr++;
+  endfunction
+endclass
+
+module example;
+  initial begin
+    parent_class p_c = new(5);
+    child_class c_c = new(10);
+    // variable declared as protected cannot be accessed outside the class
+    p_c.tmp_addr = 10;  
+    p_c.display();
+    c_c.incr_addr();// accessing protected variable in extended class
+    c_c.display();
+  end
+endmodule
