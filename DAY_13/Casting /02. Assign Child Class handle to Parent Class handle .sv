06cbbe4f3a78307dfc0a1f71ff8b0c7011e6efd4
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 22:01:55 +0530

    Update 02. Assign Child Class handle to Parent Class handle .sv

--- DAY_13/Casting /02. Assign Child Class handle to Parent Class handle .sv
+++ DAY_13/Casting /02. Assign Child Class handle to Parent Class handle .sv
@@ -1,1 +1,30 @@
+class parent_class;
+  bit [31:0] addr;
+  function void display();
+    $display("Addr = %0d",addr);
+  endfunction
+endclass
 
+class child_class extends parent_class;
+  bit [31:0] data;
+  function void display();
+    super.display();
+    $display("Data = %0d",data);
+  endfunction
+endclass
+
+module example;
+  parent_class p = new();
+  child_class c = new();
+  initial begin
+    c.addr = 5;
+    c.data = 7;
+    p = c;       // assign child class to parent class are allowed
+//    c = p;       // assign parent class to child class are not allowed
+    c.display();
+  end
+endmodule    
+
+// OUTPUT
+Addr = 5
+Data = 7
