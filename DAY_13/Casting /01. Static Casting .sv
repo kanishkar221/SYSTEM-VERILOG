affd35bfe1497087332307ed9bf0f6f67afe9372
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 21:56:16 +0530

    Update 01. Static Casting .sv

--- DAY_13/Casting /01. Static Casting .sv
+++ DAY_13/Casting /01. Static Casting .sv
@@ -1,1 +1,14 @@
+module casting;
+  real r;
+  int i;
+  initial begin
+    r = (11 * 2);
+    i = int'(11* 2);
+    $display("Real value is %f",r);
+    $display("Int value is %0d",i);
+  end
+endmodule
 
+// OUTPUT
+Real value is 22.000000
+Int value is 22
