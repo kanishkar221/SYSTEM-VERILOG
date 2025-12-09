647a10d53e9984144490f5670df41648ed595ea3
KANISHKA R <kanishka.r221@gmail.com>
2025-12-09 22:29:11 +0530

    Update Scope Resolution Operator .sv

--- DAY_14/Scope Resolution Operator .sv
+++ DAY_14/Scope Resolution Operator .sv
@@ -1,1 +1,20 @@
+class packet;
+  bit [31:0] addr;
+  static bit [31:0] id;
+  
+  function display(bit[31:0]a,b);
+    $display("Values are %0d %0d",a,b);
+  endfunction
+endclass
 
+module example;
+  int id = 10;
+  initial begin
+    packet p = new();
+    packet :: id = 20;
+    p.display(packet :: id,id);
+  end
+endmodule
+
+// OUTPUT
+Values are 20 10
