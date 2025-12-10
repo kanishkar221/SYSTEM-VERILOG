6a09a1b74a3d3a1436c84b465eeca5aff5cdeab0
KANISHKA R <kanishka.r221@gmail.com>
2025-12-10 10:28:19 +0530

    Update 02. External Task .sv

--- DAY_14/Extern Methods /02. External Task .sv
+++ DAY_14/Extern Methods /02. External Task .sv
@@ -1,1 +1,23 @@
+class packet;
+  bit [31:0] addr;
+  bit [31:0] data;
+  // task declaration - extern indicates out of body declaration
+  extern virtual task display();
+endclass
+// task implement outside class body    
+task packet::display();
+  $display("Addr = %0d,Data = %0d",addr,data);
+endtask
+ 
+module example;
+  initial begin
+    packet p;
+    p = new();
+    p.addr = 10;
+    p.data = 20;
+    p.display();
+  end
+endmodule
 
+// OUTPUT
+Addr = 10,Data = 20
