130d26c9dff0bb99d883285d40ef0789e7c1300c
KANISHKA R <kanishka.r221@gmail.com>
2025-12-10 21:09:31 +0530

    Update 01. with typedef .sv

--- DAY_14/Typedef/01. with typedef .sv
+++ DAY_14/Typedef/01. with typedef .sv
@@ -1,1 +1,23 @@
+//typedef class c1;
+typedef class c2;
+  
+// class 1
+class c1; 
+  c2 c;    // c2 is forward declared, so its valid
+endclass
 
+// class 2
+class c2;
+  c1 c;   // c1 is forward declared, so its valid
+endclass
+
+module typedef_example;
+  initial begin
+    c1 class1 = new();
+    c2 class2 = new();
+    $display("Inside Typedef");
+  end
+endmodule
+
+// OUTPUT
+Inside Typedef
