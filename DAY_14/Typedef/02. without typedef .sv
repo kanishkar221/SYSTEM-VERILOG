31019f1f1b86c86d2f4d6fc8934b447f83b2a76e
KANISHKA R <kanishka.r221@gmail.com>
2025-12-10 21:12:15 +0530

    Update 02. without typedef .sv

--- DAY_14/Typedef/02. without typedef .sv
+++ DAY_14/Typedef/02. without typedef .sv
@@ -1,1 +1,20 @@
+// class 1
+class c1;
+  c2 c;
+endclass
 
+// class 2
+class c2;
+  c1 c;
+endclass
+
+module typedef_example;
+  initial begin
+    c1 class1;
+    c2 class2;
+    $display("Inside Typedef");
+  end
+endmodule
+
+// OUTPUT
+Compile error
