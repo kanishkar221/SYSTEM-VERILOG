5b0770d73c39da30e0be9d0ca3e95399aa210d0b
KANISHKA R <kanishka.r221@gmail.com>
2025-12-10 10:27:05 +0530

    Update 01. External Function .sv

--- DAY_14/Extern Methods /01. External Function .sv
+++ DAY_14/Extern Methods /01. External Function .sv
@@ -1,1 +1,23 @@
+class packet;
+  bit [31:0] addr;
+  bit [31:0] data;
+  // function declaration - extern indicates out of body declaration
+  extern virtual function void display();
+endclass
+// function implement outside class body    
+function void packet::display();
+  $display("Addr = %0d,Data = %0d",addr,data);
+endfunction
+ 
+module example;
+  initial begin
+    packet p;
+    p = new();
+    p.addr = 10;
+    p.data = 20;
+    p.display();
+  end
+endmodule
 
+// OUTPUT
+Addr = 10,Data = 20
