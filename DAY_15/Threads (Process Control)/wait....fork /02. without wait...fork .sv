200c252b80c80cd0acb2a67cc9cf2c8985378941
KANISHKA R <kanishka.r221@gmail.com>
2025-12-11 21:44:45 +0530

    Update 02. without wait...fork .sv

--- DAY_15/Threads (Process Control)/wait....fork /02. without wait...fork .sv
+++ DAY_15/Threads (Process Control)/wait....fork /02. without wait...fork .sv
@@ -1,1 +1,30 @@
+// without using wait_fork
+module wait_fork;
+  initial begin
+    $display("---------------------------------------------");
+    fork
+      // Process-1
+      begin
+        $display($time,"\t Process 1 Started");
+        #5;
+        $display($time,"\t Process 1 Finished");
+      end
+      // Process-2
+      begin
+        $display($time,"\t Process 2 Started");
+        #20;
+        $display($time,"\t Process 2 Finished");
+      end
+    join_any
+    //wait fork;
+      $display("-------------------------------------------");
+    $finish;
+  end
+endmodule
 
+// OUTPUT
+---------------------------------------------
+    0	 Process 1 Started
+    0	 Process 2 Started
+    5	 Process 1 Finished
+-------------------------------------------
