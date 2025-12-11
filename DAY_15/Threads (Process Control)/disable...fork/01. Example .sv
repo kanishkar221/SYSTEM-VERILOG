df3df718c2b75d47d5cbc7617557db6ad2249675
KANISHKA R <kanishka.r221@gmail.com>
2025-12-11 21:52:24 +0530

    Update 01. Example .sv

--- DAY_15/Threads (Process Control)/disable...fork/01. Example .sv
+++ DAY_15/Threads (Process Control)/disable...fork/01. Example .sv
@@ -1,1 +1,48 @@
+module disable_fork;
+  initial begin
+    $display("------------------------------------------------------");
+    // fork 1
+    fork
+      // Process 1
+      begin
+        $display($time,"\t Process-1 of fork-1 Started");
+        #5;
+        $display($time,"\t Process-1 of fork-1 Finished");
+      end
+      // Process 2
+      begin
+        $display($time,"\t Process-2 of fork-1 Started");
+        #20;
+        $display($time,"\t Process-2 of fork-1 Finished");
+      end
+    join_any
+    // fork 2
+    fork
+      // Process 1
+      begin
+        $display($time,"\t Process-1 of fork-2 Started");
+        #5;
+        $display($time,"\t Process-1 of fork-2 Finished");
+      end
+      // Process 2
+      begin
+        $display($time,"\t Process-2 of fork-2 Started");
+        #20;
+        $display($time,"\t Process-2 of fork-2 Finished");
+      end
+    join_none
+    disable fork;
+    $display("------------------------------------------------------");
+    $display($time,"\t After Disable Fork");
+    $display("------------------------------------------------------");
+  end
+endmodule
 
+// OUTPUT
+------------------------------------------------------
+    0	 Process-1 of fork-1 Started
+    0	 Process-2 of fork-1 Started
+    5	 Process-1 of fork-1 Finished
+------------------------------------------------------
+    5	 After Disable Fork
+------------------------------------------------------
