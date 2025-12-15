d374e9646580cf5bfd8e2e746ffcc108270e9e9a
KANISHKA R <kanishka.r221@gmail.com>
2025-12-15 22:17:59 +0530

    Update 03. Merging event .sv

--- DAY_16/Events/03. Merging event .sv
+++ DAY_16/Events/03. Merging event .sv
@@ -1,1 +1,22 @@
+module merge_events;
+  event a,b;
+  initial begin
+    fork
+      #50 -> a;
+      #30 -> b;
+      #20 b = a;
+      begin
+        wait(a.triggered);
+        $display("%0t event a is done",$time);
+      end
+      begin
+        wait(b.triggered);
+        $display("%0t event b is done",$time);
+      end
+    join
+  end
+endmodule
 
+// OUTPUT
+30 event a is done
+30 event b is done
