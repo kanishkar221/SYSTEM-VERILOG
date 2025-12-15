c7cb71ef7814dc8f147bdb301fc9313df770fce7
KANISHKA R <kanishka.r221@gmail.com>
2025-12-15 22:21:47 +0530

    Add non-blocking event example in SystemVerilog
    Demonstrate non-blocking event handling using both wait and @.

--- DAY_16/Events/05. Non Blocking event .sv
+++ DAY_16/Events/05. Non Blocking event .sv
@@ -1,1 +1,24 @@
+module non_blocking_event;
+  event BJT;
+  initial begin
+    $display($time,"\t triggering the event");
+    ->> BJT;
+  end
+  initial begin
+    $display($time,"\t waiting for the event using wait");
+    wait(BJT.triggered);
+    $display($time,"\t event BJT is triggered using wait");
+  end
+  initial begin
+    $display($time,"\t waiting for the event using @");
+    @(BJT.triggered);
+    $display($time,"\t event BJT is triggered using @");
+  end
+endmodule
 
+// OUTPUT
+      0	 triggering the event
+      0	 waiting for the event using wait
+      0	 waiting for the event using @
+      0	 event BJT is triggered using wait
+      0	 event BJT is triggered using @
