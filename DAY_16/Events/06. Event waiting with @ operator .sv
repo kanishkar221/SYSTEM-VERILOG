b5798b2a476c173d93387b7e73dbe0aa5d8c4cdb
KANISHKA R <kanishka.r221@gmail.com>
2025-12-15 22:24:37 +0530

    Update 06. Event waiting with @ operator .sv

--- DAY_16/Events/06. Event waiting with @ operator .sv
+++ DAY_16/Events/06. Event waiting with @ operator .sv
@@ -1,1 +1,24 @@
+module event_example;
+  event e1;
+  initial begin
+    fork
+      // Process 1
+      begin
+        #40;
+        $display($time,"\t Triggering the event");
+        ->e1;
+      end
+      // Process 2
+      begin
+        $display($time,"\t Waiting for the event to trigger");
+        @(e1.triggered);
+        $display($time,"\t Event triggered");
+      end
+    join
+  end
+endmodule
 
+// OUTPUT
+      0	 Waiting for the event to trigger
+      40	 Triggering the event
+      40	 Event triggered
