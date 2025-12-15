50b3e6708c75310cafd4b4a241b757526842fa5c
KANISHKA R <kanishka.r221@gmail.com>
2025-12-15 22:13:18 +0530

    Update 01. @ and wait operator .sv

--- DAY_16/Events/01. @ and wait operator .sv
+++ DAY_16/Events/01. @ and wait operator .sv
@@ -1,1 +1,25 @@
+module wait_example;
+  event e;
+  initial begin
+    $display($time,"\t triggering the event");
+    -> e;
+  end
+  
+  initial begin
+    $display($time,"\t waiting for the event using wait");
+    wait(e.triggered);
+    $display($time,"\t event is triggered using wait");
+  end
+  
+  initial begin
+    $display($time,"\t waiting for the event using @");
+    @(e.triggered);
+    $display($time,"\t event is triggered using @");
+  end
+endmodule
 
+// OUTPUT
+      0	 triggering the event
+      0	 waiting for the event using wait
+      0	 event is triggered using wait
+      0	 waiting for the event using @
