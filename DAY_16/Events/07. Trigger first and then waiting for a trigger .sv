486858f7c9f493bee9427cbfadc80454ad37ca68
KANISHKA R <kanishka.r221@gmail.com>
2025-12-15 22:26:38 +0530

    Implement event triggering and waiting example
    Added a module that demonstrates event triggering and waiting in SystemVerilog.

--- DAY_16/Events/07. Trigger first and then waiting for a trigger .sv
+++ DAY_16/Events/07. Trigger first and then waiting for a trigger .sv
@@ -1,1 +1,30 @@
+module event_example;
+  event e1;
+  initial begin
+    fork
+      // Process 1,triggers the event
+      begin
+        #40;
+        $display($time,"\t Triggering the event");
+        ->e1;
+      end
+      // Process 2,wait for the event to trigger
+      begin
+        $display($time,"\t Waiting for the event to trigger");
+        #60;
+        @(e1.triggered);
+        $display($time,"\t Event Triggered");
+      end
+    join
+  end
+  initial begin
+    #100;
+    $display($time,"\t Ending the simulation");
+    $finish;
+  end
+endmodule
 
+// OUTPUT
+      0	 Waiting for the event to trigger
+      40	 Triggering the event
+      100	 Ending the simulation
