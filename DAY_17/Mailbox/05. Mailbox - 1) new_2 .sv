bc8187fb77906c87be5dce24bbd1af07b69ab2c4
KANISHKA R <kanishka.r221@gmail.com>
2025-12-17 22:27:05 +0530

    Update 05. Mailbox - 1) new_2 .sv

--- DAY_17/Mailbox/05. Mailbox - 1) new_2 .sv
+++ DAY_17/Mailbox/05. Mailbox - 1) new_2 .sv
@@ -1,1 +1,56 @@
+class A;
+  int a;
+  int i;
+  mailbox m;
+  
+  function new(mailbox m1);
+    this.m = m1;
+  endfunction
+  
+  function void check();
+    if(m == null)begin
+      $display("Mailbox is not created");
+    end
+    else
+      $display("Mailbox is created");
+  endfunction
+  
+  task trans_data();
+    for(i=0;i<5;i++)begin
+      m.put(a);
+      a++;
+      $display("Value of a = %0d",a);
+    end
+    $display("No of messages in mailbox = %0d",m.num());
+  endtask
+endclass
 
+module tb();
+  A a1;
+  mailbox main = new();
+  initial begin
+    a1 = new(main);
+    a1.check();
+    repeat(2) begin
+      $display("--------------------------------");
+      a1.trans_data();
+    end
+  end
+endmodule
+
+// OUTPUT
+Mailbox is created
+--------------------------------
+Value of a = 1
+Value of a = 2
+Value of a = 3
+Value of a = 4
+Value of a = 5
+No of messages in mailbox = 5
+--------------------------------
+Value of a = 6
+Value of a = 7
+Value of a = 8
+Value of a = 9
+Value of a = 10
+No of messages in mailbox = 10
