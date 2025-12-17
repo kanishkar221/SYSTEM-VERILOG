5fa78eb37fae3ccad26ea01a9eb3fe8ce903214e
KANISHKA R <kanishka.r221@gmail.com>
2025-12-17 22:22:41 +0530

    Update 04. Mailbox - 1) new_1 .sv

--- DAY_17/Mailbox/04. Mailbox - 1) new_1 .sv
+++ DAY_17/Mailbox/04. Mailbox - 1) new_1 .sv
@@ -1,1 +1,36 @@
+class A;
+  int a;
+  int i;
+  mailbox m;
+  function new(mailbox m1);
+    this.m = m1;
+  endfunction
+  
+  task check();
+    if(m == null)begin
+      $display("Mailbox is not corrected");
+    end
+    else
+      $display("Mailbox is created");
+    for(i=0;i<3;i++)begin
+      a++;
+      m.put(a);
+      $display("Value of a = %0d",a);
+    end
+  endtask
+endclass
 
+module tb();
+  A a1;
+  mailbox main = new(3);
+  initial begin
+    a1 = new(main);
+	a1.check();
+  end
+endmodule
+
+// OUTPUT
+Mailbox is created
+Value of a = 1
+Value of a = 2
+Value of a = 3
