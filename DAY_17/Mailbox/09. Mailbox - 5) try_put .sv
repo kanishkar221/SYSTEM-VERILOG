79116c70a44e94ca544e1b2ce865e14fe4bce8dc
KANISHKA R <kanishka.r221@gmail.com>
2025-12-18 21:26:03 +0530

    Update 09. Mailbox - 5) try_put .sv

--- DAY_17/Mailbox/09. Mailbox - 5) try_put .sv
+++ DAY_17/Mailbox/09. Mailbox - 5) try_put .sv
@@ -1,1 +1,69 @@
+class A;
+  int a;
+  int i;
+  mailbox m;
+  
+  function new(mailbox m1);
+    this.m = m1;
+  endfunction
+  
+  task trans_data();
+    for(i=0;i<4;i++)begin
+      a++;
+      if(m.try_put(a))
+        $display("[%0t] 1.Transmitter:Value of a = %0d",$time,a);
+      else
+        $display("Failed while trying to put data = %0d",a);
+    end
+    $display("------------------------------------");
+  endtask
+endclass
 
+class B;
+  int a;
+  int i;
+  mailbox m;
+  
+  function new(mailbox m2);
+    this.m = m2;
+  endfunction
+  
+  task recei_data();
+    repeat(3)begin
+      m.get(a);
+      $display("[%0t] 2.Receiver:Value of a = %0d",$time,a);
+    end
+  endtask
+endclass
+
+module tb();
+  A a1;
+  B b1;
+  mailbox main = new(3);
+  initial begin
+    a1 = new(main);
+    b1 = new(main);
+    repeat(2)begin
+      a1.trans_data();
+      b1.recei_data();
+    end
+  end
+endmodule
+
+// OUTPUT
+[0] 1.Transmitter:Value of a = 1
+[0] 1.Transmitter:Value of a = 2
+[0] 1.Transmitter:Value of a = 3
+Failed while trying to put data = 4
+------------------------------------
+[0] 2.Receiver:Value of a = 1
+[0] 2.Receiver:Value of a = 2
+[0] 2.Receiver:Value of a = 3
+[0] 1.Transmitter:Value of a = 5
+[0] 1.Transmitter:Value of a = 6
+[0] 1.Transmitter:Value of a = 7
+Failed while trying to put data = 8
+------------------------------------
+[0] 2.Receiver:Value of a = 5
+[0] 2.Receiver:Value of a = 6
+[0] 2.Receiver:Value of a = 7
