804037d4c0f878b75116cd5eabab1eda98470cf4
KANISHKA R <kanishka.r221@gmail.com>
2025-12-30 21:46:35 +0530

    Update 01. Inverted inside constraint .sv

--- DAY_18/Randomization/7. Inverted Inside Constraint/01. Inverted inside constraint .sv
+++ DAY_18/Randomization/7. Inverted Inside Constraint/01. Inverted inside constraint .sv
@@ -1,1 +1,26 @@
+// Invert inside constraint
+class invert;
+  rand bit [3:0] var1;
+  constraint c1 {!(var1 inside {[3:9]});}
+endclass
 
+module top;
+  initial begin
+    int i;
+    invert c = new();
+    $display("Output for invert inside constraint");
+    for (int i=1;i<7;i++) begin
+      void'(c.randomize());
+      $display("[%0t] @ iteration:%0d Var1 = %0d",$time,i,c.var1);
+    end
+  end
+endmodule     
+
+// OUTPUT
+Output for invert inside constraint
+[0] @ iteration:1 Var1 = 1
+[0] @ iteration:2 Var1 = 13
+[0] @ iteration:3 Var1 = 2
+[0] @ iteration:4 Var1 = 13
+[0] @ iteration:5 Var1 = 1
+[0] @ iteration:6 Var1 = 0
