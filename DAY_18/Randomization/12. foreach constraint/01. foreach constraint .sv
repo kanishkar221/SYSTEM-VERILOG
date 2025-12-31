91e123586a668d4fa3ed7d0b95983f2db38c59e7
KANISHKA R <kanishka.r221@gmail.com>
2025-12-31 21:58:17 +0530

    Update 01. foreach constraint .sv

--- DAY_18/Randomization/12. foreach constraint/01. foreach constraint .sv
+++ DAY_18/Randomization/12. foreach constraint/01. foreach constraint .sv
@@ -1,1 +1,23 @@
+class value;
+  rand bit [3:0] s_array1[4];
+  rand bit [3:0] s_array2[5];
+  constraint cons {
+    foreach (s_array1[i])
+      s_array1[i] == i;
+    foreach (s_array2[i])
+      s_array2[i] == i;
+  }
+endclass
 
+module top;
+  initial begin
+    value val = new();
+    void'(val.randomize());
+    $display("\t s_array1 = %0p",val.s_array1);
+    $display("\t s_array2 = %0p",val.s_array2);
+  end
+endmodule
+
+// OUTPUT
+s_array1 = 0 1 2 3
+s_array2 = 0 1 2 3 4
