85ecd9a1cfe5fa2af56cf23965b29498e4f44e8a
KANISHKA R <kanishka.r221@gmail.com>
2026-01-05 22:30:07 +0530

    Update 01. Only inline constraint .sv

--- DAY_18/Randomization/17. Inline Constraint/01. Only inline constraint .sv
+++ DAY_18/Randomization/17. Inline Constraint/01. Only inline constraint .sv
@@ -1,1 +1,20 @@
+// Only inline constraint
+class generator;
+  rand bit [2:0] value;  // 3 bit random variable
+endclass
 
+module only_inline();
+  generator c = new();
+  initial begin
+    //  Loop to perform randomization twice
+    for(int i=1;i<=2;i++) begin
+      // Apply an inline constraint to force value to 3
+      void'(c.randomize()with{value == 3;});
+      #1 $display("\t[%0d] Iteration:%0d Value:%0d",$time,i,c.value);
+    end
+  end
+endmodule
+
+// OUTPUT
+[1] Iteration:1 Value:3
+[2] Iteration:2 Value:3
