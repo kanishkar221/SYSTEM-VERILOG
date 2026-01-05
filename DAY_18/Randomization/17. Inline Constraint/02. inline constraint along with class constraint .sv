d5555eee8da435bc0becf4e8f5650b1dc61d2a0a
KANISHKA R <kanishka.r221@gmail.com>
2026-01-05 22:32:32 +0530

    Update 02. inline constraint along with class constraint .sv

--- DAY_18/Randomization/17. Inline Constraint/02. inline constraint along with class constraint .sv
+++ DAY_18/Randomization/17. Inline Constraint/02. inline constraint along with class constraint .sv
@@ -1,1 +1,25 @@
+// inline constraint along with class constraint
+class generator;
+  rand bit [2:0] value;   // 3 bit random variable
+  // Constraint:Ensure value is always greater than 2
+  constraint cons {value > 2;}
+endclass
+
+module inline_constraint();
+  generator c = new();
+  initial begin
+    // Perform randomization using class constraint
+    void'(c.randomize());
+    #1 $display("\t[%0t] Iteration:1 value:%0d",$time,c.value);
+    // Apply inline constraint to force value to 7
+    void'(c.randomize()with{value == 7;});
+    #1 $display("\t[%0t] Iteration:2 value:%0d",$time,c.value);
+  end
+endmodule
+
+// OUTPUT
+[1] Iteration:1 value:5
+[2] Iteration:2 value:7
+// The initial constraint in class is value >2 so its first iteration value is in the range of 2 to 7. 
+// But in the second iteration giving value must be 7 from inline so the value is 7.
 
