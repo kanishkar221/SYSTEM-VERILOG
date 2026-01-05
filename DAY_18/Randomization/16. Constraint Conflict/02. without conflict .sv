f3b5ed487795638b0e984f5cabe03289ac6d4ce3
KANISHKA R <kanishka.r221@gmail.com>
2026-01-05 22:25:51 +0530

    Update 02. without conflict .sv

--- DAY_18/Randomization/16. Constraint Conflict/02. without conflict .sv
+++ DAY_18/Randomization/16. Constraint Conflict/02. without conflict .sv
@@ -1,1 +1,24 @@
+// without conflict
+class class1;     
+  rand bit [3:0] a;
+  constraint c1 {a < 10;}
+  constraint c2 {a > 5;}
+endclass
 
+module conflict;
+  class1 c = new();
+  initial begin
+    if (c.randomize()) begin
+      $display("Randomize Successfully");
+      $display("Randomize value of a = %0d",c.a);
+    end
+    else begin
+      $display("Randomize fail because conflict occurs");
+      $display("Value of a = %0d",c.a);
+    end
+  end
+endmodule 
+
+// OUTPUT
+Randomize Successfully
+Randomize value of a = 9
