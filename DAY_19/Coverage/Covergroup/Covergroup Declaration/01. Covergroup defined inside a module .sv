82c637765fa1ee9e6e918ba41c31085c234c4b9b
KANISHKA R <kanishka.r221@gmail.com>
2026-01-20 22:16:46 +0530

    Update 01. Covergroup defined inside a module .sv

--- DAY_19/Coverage/Covergroup/Covergroup Declaration/01. Covergroup defined inside a module .sv
+++ DAY_19/Coverage/Covergroup/Covergroup Declaration/01. Covergroup defined inside a module .sv
@@ -1,1 +1,40 @@
+// 1. Covergroup defined inside a module
+class pack;
+  rand bit [1:0] a; // 2-bit random variable
+  rand bit [2:0] b; // 3-bit random variable
+endclass
 
+module cvgrp_inside_mod;
+  pack p; // Declare an instance of class pack
+
+  // Define covergroup with arguments (cannot directly use class members)
+  covergroup cgrp(ref bit [1:0] a, ref bit [2:0] b);
+    c1: coverpoint a; // Coverpoint for a
+    c2: coverpoint b; // Coverpoint for b
+  endgroup
+
+  cgrp c; // Declare covergroup instance
+
+  initial begin
+    p = new();   // Instantiate class pack
+    c = new(p.a, p.b); // Instantiate covergroup, passing class variables as references
+
+    repeat (5) begin
+      void'(p.randomize()); // Randomize class variables
+
+      c.sample(); // Manually sample coverage
+
+      // Display values and current instance coverage percentage
+      $display("a=%d ; b=%d ; coverage %%= %.2f%%", p.a, p.b, c.get_inst_coverage());
+    end
+
+    $finish; 
+  end
+endmodule
+
+// OUTPUT
+a=3 ; b=4 ; coverage %= 18.75%
+a=1 ; b=6 ; coverage %= 37.50%
+a=2 ; b=3 ; coverage %= 56.25%
+a=2 ; b=3 ; coverage %= 56.25%
+a=2 ; b=0 ; coverage %= 62.50%
