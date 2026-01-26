0609f96fd0d5d2fd0e159d8abd06e3565b4834fd
KANISHKA R <kanishka.r221@gmail.com>
2026-01-26 21:31:07 +0530

    Update 03. Set of transition .sv

--- DAY_19/Coverage/Coverage Bins/03. Transition bins/03. Set of transition .sv
+++ DAY_19/Coverage/Coverage Bins/03. Transition bins/03. Set of transition .sv
@@ -1,1 +1,38 @@
+// 03. Set of transition 
+module single_val_trans_bin;
+  // Declare 4 bit variable 'a'
+  bit [3:0] a;
+  // Declare dynamic array with predefined values
+  bit [2:0] values[$] = '{1,2,3,4,5};
+  
+  // Define covergroup to track specific transition sequences of 'a'
+  covergroup cov_grp;
+    c1 : coverpoint a {
+      bins tran_1 = (1,2 => 3,4); // Transition from (1 or 2) to (3 or 4)
+      bins tran_2 = (3,4 => 5);   // Transition from (3 or 4) to 5
+      bins tran_3 = (1,3 => 4);   // Transition from (1 or 3) to 4
+    }
+  endgroup
+  
+  // Instance of covergroup
+  cov_grp cg = new();
+  
+  initial begin
+    // Iterate over each value in the array and sample coverage
+    foreach(values[i])
+      begin
+        a = values[i];            // Assign value from array
+        cg.sample();              // Sample coverage
+        
+        // Display current value and coverage percentage
+        $display("val = %d, cov = %.2f %%", a, cg.get_inst_coverage());
+      end
+  end
+endmodule
 
+// OUTPUT
+val =  1, cov = 0.00 %
+val =  2, cov = 0.00 %
+val =  3, cov = 33.33 %
+val =  4, cov = 66.67 %
+val =  5, cov = 100.00 %
